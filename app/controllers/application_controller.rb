ddc62bcec093e48cdb49f650c7f9fe73f7603ce8
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-21 16:08:08 +1200

    NGC-15: UAT environment is secured with HTTP basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :http_basic_auth
+
+  private
+
+  def http_basic_auth
+    return unless ENV["HTTP_BASIC_AUTH_USERNAME"] && ENV["HTTP_BASIC_AUTH_PASSWORD"]
+
+    authenticate_or_request_with_http_basic("Username and Password please") do |username, password|
+      username == ENV["HTTP_BASIC_AUTH_USERNAME"] && password == ENV["HTTP_BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
