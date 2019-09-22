26fd28856b244ed2a4c1a77ce7a95dab536d452b
Trevor Hose <trevor.hose@ackama.com>
2019-09-22 19:19:47 +1200

    NCG-18: Implementation of controller index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,15 @@
 
 class SearchController < ApplicationController
   def index
+    @results = SearchService.call(search: new_search).data
+    render template: "home/results"
   end
 
   private
+
+  def new_search
+    Search.new(search_params)
+  end
 
   def search_params
     params.require(:search).permit(:word)
