c0c4ed8f1dc97495429defdbc6bf81a578659eb9
Trevor Hose <trevor.hose@ackama.com>
2019-10-01 15:38:11 +1300

    Update search model to include page methods

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,6 +4,8 @@ class Search
   include ActiveModel::Model
 
   DIRECTION = /\A(0|1)\Z/.freeze
+  PAGE = /\A[0-9]{1,2}\Z/.freeze
+  LIMIT = 16
 
   attr_reader :word, :published
 
@@ -21,5 +23,40 @@ class Search
                 end
 
     @published = direction
+  end
+
+  def page
+    @page || build_page(LIMIT)
+  end
+
+  def page=(value)
+    limit = if value.to_s.match(PAGE).blank?
+              LIMIT
+            else
+              LIMIT * value.to_i
+            end
+
+    @page = build_page(limit)
+  end
+
+  private
+
+  def build_page(limit)
+    page = (limit.to_f / LIMIT).ceil
+    {
+      this_page: page,
+      next_page: page + 1,
+      limit: limit,
+      word: word,
+      next_pub: fetch_published
+    }
+  end
+
+  def fetch_published
+    return "0" if published.nil?
+
+    return "1" if published.downcase == "asc"
+
+    "0"
   end
 end
