f807446cecf24cb431a0b41d05f4bbfbf2ddfe9d
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-03 15:45:56 +1300

    Generate topic when a sign is created

--- spec/factories/sign.rb
+++ spec/factories/sign.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     english   { Faker::Name.first_name   }
     maori     { Faker::Name.last_name    } # until we get some proper data
     secondary { Faker::Name.middle_name  }
+    association :topic
   end
 end
