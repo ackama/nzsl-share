b8e41c48d53510fc04d2f343329f1a4d24821a39
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-03 15:46:25 +1300

    Add basic sign show page

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -1,6 +1,6 @@
 class SignsController < ApplicationController
   def show
-    @sign = policy_scope(Sign.includes(:contributor, :topic, :tags))
+    @sign = policy_scope(Sign.includes(:contributor, :topic))
             .find(params[:id])
     authorize @sign
     return unless stale?(@sign)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq" # monitoring console
 
   root "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :search, only: [:index]
+  resources :signs, only: [:show]
 end
