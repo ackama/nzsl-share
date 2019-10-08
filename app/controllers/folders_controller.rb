a7643ad8c2632961904c4aeed0a2f06932115e67
Trevor Hose <trevor.hose@ackama.com>
2019-10-09 11:41:53 +1300

    Update ctrl to redirect to show from share
    * check the shape of a uuid

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -5,6 +5,20 @@ class FoldersController < ApplicationController
     authorize folders
   end
 
+  def show
+    @folder = if uuid?
+                policy_scope(Folder).find_by(share_token: id)
+              else
+                policy_scope(Folder).find_by(id: id)
+              end
+
+    authorize @folder
+
+    respond_to do |format|
+      format.html { render :show }
+    end
+  end
+
   def new
     @folder = Folder.new
     authorize @folder
@@ -40,7 +54,21 @@ class FoldersController < ApplicationController
                 (@folder.destroy ? { notice: t(".success") } : { alert: t(".failure") })
   end
 
+  def share
+    @folder = policy_scope(Folder).find_by(id: id)
+    authorize @folder
+    @folder.update(share_token: SecureRandom.uuid)
+
+    if @folder.errors.empty?
+      redirect_to action: "show", id: @folder.share_token
+    else
+      render :new
+    end
+  end
+
   private
+
+  UUID_SHAPE = /\A[a-z\d]{8}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{12}\Z/i.freeze
 
   def folders
     @folders = policy_scope(Folder).order("title ASC")
@@ -57,5 +85,13 @@ class FoldersController < ApplicationController
       format.js { render inline: "location.reload();" }
       format.html { redirect_to folders_path }
     end
+  end
+
+  def id
+    params[:id]
+  end
+
+  def uuid?
+    id.to_s.match(UUID_SHAPE).present?
   end
 end
