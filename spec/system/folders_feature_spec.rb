04409119cd243c9c172d41044a4a35fe0f8c732e
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-09 12:26:24 +1300

    Add shared examples for creation and editing for folder system tests

--- spec/system/folders_feature_spec.rb
+++ spec/system/folders_feature_spec.rb
@@ -2,26 +2,64 @@ require "rails_helper"
 
 RSpec.describe "Folders", type: :system, uses_javascript: true do
   let(:process) { FolderFeature.new }
-  before { process.start }
-
-  it_behaves_like "an accessible page"
 
   describe "Creating a new folder" do
-    it "can add a new folder successfully" do
-      title = "My Folder"
-      description = "Signs I like"
-      process.click_create_new
-      process.enter_title(title)
-      process.enter_description(description)
-      process.submit_new_folder_form
-      expect(process).to have_content "Folder successfully created."
-      expect(process).to have_content(title)
+    before { process.start }
+
+    shared_examples "creating a folder" do
+      it "can add a new folder successfully" do
+        title = "My Folder"
+        description = "Signs I like"
+        process.click_create_new
+        process.enter_title(title)
+        process.enter_description(description)
+        process.submit_new_folder_form
+        expect(process).to have_content "Folder successfully created."
+        expect(process).to have_content(title)
+      end
+
+      it "displays correct validation errors" do
+        process.click_create_new
+        process.submit_new_folder_form
+        expect(process).to have_content "can't be blank"
+      end
     end
 
-    it "displays correct validation errors" do
-      process.click_create_new
-      process.submit_new_folder_form
-      expect(process).to have_content "can't be blank"
+    context "using a modal", uses_javascript: true do
+      it_behaves_like "creating a folder"
+    end
+
+    it_behaves_like "creating a folder", uses_javascript: false
+  end
+
+  describe "Editing a folder" do
+    let!(:folder) { FactoryBot.create(:folder, user: process.user) }
+    before { process.start }
+
+    shared_examples "editing a folder" do
+      it "updates the folder successfully" do |example|
+        title = folder.title
+        process.edit_folder(dropdown: example.metadata[:uses_javascript])
+        process.enter_title(title + " CHANGED")
+        process.submit_edit_folder_form
+        expect(process).to have_content "Folder successfully updated."
+        expect(process).to have_content(title + " CHANGED")
+      end
+
+      it "displays correct validation errors" do |example|
+        process.edit_folder(dropdown: example.metadata[:uses_javascript])
+        process.enter_title("")
+        process.submit_edit_folder_form
+        expect(process).to have_content "can't be blank"
+      end
+    end
+
+    context "inside modal", uses_javascript: true do
+      it_behaves_like "editing a folder"
+    end
+
+    context "without JS" do
+      it_behaves_like "editing a folder"
     end
   end
 end
