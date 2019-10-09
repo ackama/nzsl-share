f447e77dd86cacae87c6c8c9274fb31dbd237adb
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-10 11:06:35 +1300

    Remove unused scope that could cause performance problems if used

--- app/models/folder_membership.rb
+++ app/models/folder_membership.rb
@@ -1,10 +1,4 @@
 class FolderMembership < ApplicationRecord
   belongs_to :folder, counter_cache: :signs_count
   belongs_to :sign
-
-  def self.for(sign, user=nil)
-    FolderMembership
-      .where(sign: sign)
-      .yield_self { |sp| user.nil? ? sp : sp.includes(:folder).where(folders: { user_id: user.id }) }
-  end
 end
