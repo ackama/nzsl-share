01016f70229a7be1d1acb0b929ff7fdd11afe292
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-10 11:47:16 +1300

    Add specs to cover sign presenter methods
    Including necessary fixes for AbcSize to presenter and to handle non-logged-in users

--- app/presenters/sign_presenter.rb
+++ app/presenters/sign_presenter.rb
@@ -10,20 +10,28 @@ class SignPresenter < ApplicationPresenter
     h.localize(sign.published_at || sign.created_at, format: "%-d %B %Y")
   end
 
-  def available_folders
-    @folders ||= h.current_user_folders
+  def available_folders(&block)
+    return [] unless h.user_signed_in?
+
+    @folders ||= h.current_user.folders
     @memberships ||= sign.folder_memberships.includes(:folder).where(folder_id: @folders.map(&:id))
 
-    @folders.each_with_object({}) do |folder, acc|
-      membership = @memberships.find { |mem| mem.folder_id == folder.id }
-      yield folder, membership if block_given?
-
-      acc[folder] = membership
-    end
+    map_folders_to_memberships(@folders, @memberships, &block)
   end
 
   def assignable_folder_options
     assignable_folders = available_folders.reject { |_folder, membership| membership.present? }
     h.options_for_select(assignable_folders.map { |f, _m| [f.title, f.id] })
+  end
+
+  private
+
+  def map_folders_to_memberships(folders, memberships)
+    folders.each_with_object({}) do |folder, acc|
+      membership = memberships.find { |mem| mem.folder_id == folder.id }
+      yield folder, membership if block_given?
+
+      acc[folder] = membership
+    end
   end
 end
--- spec/presenters/sign_presenter_spec.rb
+++ spec/presenters/sign_presenter_spec.rb
@@ -11,6 +11,62 @@ RSpec.describe SignPresenter, type: :presenter do
     end
   end
 
+  describe "#available_folders" do
+    # We need the sign to actually exist so we can set up
+    # folder associations
+    let(:sign) { FactoryBot.create(:sign) }
+    let(:user) { sign.contributor }
+    before { sign_in user }
+    subject { presenter.available_folders }
+
+    context "with no folders" do
+      it { expect(subject).to be_empty }
+    end
+
+    context "with no user signed in" do
+      before { sign_out :user }
+      it { is_expected.to be_empty }
+    end
+
+    context "when the sign is assigned to a folder" do
+      let!(:folder) { FactoryBot.create(:folder, user: user, signs: [sign]) }
+      it { expect(subject).to eq(folder => sign.folder_memberships.first) }
+    end
+
+    context "when the sign is not assigned to a folder" do
+      let!(:folder) { FactoryBot.create(:folder, user: user) }
+      it { expect(subject).to eq(folder => nil) }
+    end
+
+    context "when a block is passed" do
+      let!(:folder) { FactoryBot.create(:folder, user: user, signs: [sign]) }
+      it "yields the folder and membership to the block" do
+        expect do |b|
+          presenter.available_folders(&b)
+        end.to yield_with_args(folder, folder.folder_memberships.first)
+      end
+    end
+  end
+
+  describe "#assignable_folder_options" do
+    # We need the sign to actually exist so we can set up
+    # folder associations
+    let(:sign) { FactoryBot.create(:sign) }
+    let(:user) { sign.contributor }
+    before { sign_in user }
+    subject { presenter.assignable_folder_options }
+
+    context "when the sign is assigned to the folder" do
+      let!(:folder) { FactoryBot.create(:folder, user: user, signs: [sign]) }
+      it { is_expected.to be_empty }
+    end
+
+    context "when the sign is not assigned to a folder" do
+      let!(:folder) { FactoryBot.create(:folder, user: user) }
+      it { is_expected.to match(/\A<option value="#{folder.id}">/) }
+    end
+  end
+
   describe "#friendly_date" do
     subject { presenter.friendly_date }
     context "sign is published" do
