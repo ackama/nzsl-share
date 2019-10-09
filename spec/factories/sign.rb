d11f6af1a6486821b7bc2738bb891a1066973f5c
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-10 10:04:02 +1300

    Move published_at to a trait, since the default state of a sign should be non-published

--- spec/factories/sign.rb
+++ spec/factories/sign.rb
@@ -3,8 +3,11 @@ FactoryBot.define do
     english      { Faker::Name.first_name       }
     maori        { Faker::Name.last_name        } # until we get some proper data
     secondary    { Faker::Name.middle_name      }
-    published_at { DateTime.now - (rand * 1000) }
     association :topic
     association :contributor, factory: :user
+
+    trait :published do
+      published_at { DateTime.now - (rand * 1000) }
+    end
   end
 end
