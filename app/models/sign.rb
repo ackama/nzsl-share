f3961f4eb698962a1668bac638fbe491bc442ad3
Trevor Hose <trevor.hose@ackama.com>
2019-10-10 10:59:49 +1300

    Add search scopes

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -10,6 +10,10 @@ class Sign < ApplicationRecord
   # modify the rules later to take into account activity
   # or some other measure of popularity
   scope :preview, -> { limit(4) }
+  scope :search, ->(ids) { where(id: ids) }
+  scope :published_order, ->(direction) { order(published_at: direction) }
+  scope :default_order, -> { order(english: :asc) }
+  scope :search_limit, ->(limit) { limit(limit) }
 
   def agree_count; 0; end
   def disagree_count; 0; end
