0d61e529dca84d6231fee49e924822afade772c2
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-10 11:06:05 +1300

    Implement presenter methods for fetching folder and membership lists for a sign card

--- app/presenters/sign_presenter.rb
+++ app/presenters/sign_presenter.rb
@@ -9,4 +9,21 @@ class SignPresenter < ApplicationPresenter
   def friendly_date
     h.localize(sign.published_at || sign.created_at, format: "%-d %B %Y")
   end
+
+  def available_folders
+    @folders ||= h.current_user_folders
+    @memberships ||= sign.folder_memberships.includes(:folder).where(folder_id: @folders.map(&:id))
+
+    @folders.each_with_object({}) do |folder, acc|
+      membership = @memberships.find { |mem| mem.folder_id == folder.id }
+      yield folder, membership if block_given?
+
+      acc[folder] = membership
+    end
+  end
+
+  def assignable_folder_options
+    assignable_folders = available_folders.reject { |_folder, membership| membership.present? }
+    h.options_for_select(assignable_folders.map { |f, _m| [f.title, f.id] })
+  end
 end
