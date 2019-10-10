26c0315c69c0b7d5b238ab670314c6aa02b14d06
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-10 13:23:35 +1300

    Add RSpec examples for managing folders on a sign card with JS

--- spec/system/sign_card_feature_spec.rb
+++ spec/system/sign_card_feature_spec.rb
@@ -5,39 +5,76 @@ RSpec.describe "Sign card features", type: :system do
   let(:presenter) { SignPresenter.new(sign, ActionView::Base.new) }
   let(:authenticator) { AuthenticateFeature.new }
 
-  # Run all these examples within the context of the sign
-  # card DOM element
-  around do |example|
+  before do |example|
     authenticator.sign_in unless example.metadata[:signed_out]
-    # Topic will show the sign in a card
     visit topic_path(sign.topic)
-    within(find(".sign-card", match: :first), &example.run)
+  end
+
+  def sign_card
+    find(".sign-card", match: :first)
   end
 
   def inside_card(&block)
-    within(find(".sign-card", match: :first), &block)
+    within(sign_card, &block)
   end
 
   it "shows the title" do
-    expect(page).to have_selector ".sign-card__title", text: sign.english
+    expect(sign_card).to have_selector ".sign-card__title", text: sign.english
   end
 
   it "shows the contributor's username" do
-    expect(page).to have_content sign.contributor.username
+    expect(sign_card).to have_content sign.contributor.username
   end
 
   it "shows a formatted date" do
-    expect(page).to have_content presenter.friendly_date
+    expect(sign_card).to have_content presenter.friendly_date
   end
 
   it "shows the embedded media" do
-    expect(page).to have_selector ".sign-card__media > iframe[src^='https://player.vimeo.com']"
+    expect(sign_card).to have_selector ".sign-card__media > iframe[src^='https://player.vimeo.com']"
   end
 
-  describe "Adding & removing from folders with JS" do
-    it "shows existing folder state"
-    it "adds the sign to a folder"
-    it "removes the sign from a folder"
+  describe "Adding & removing from folders with JS", uses_javascript: true do
+    let(:folder) { FactoryBot.create(:folder, user: authenticator.user) }
+    let!(:other_folder) { FactoryBot.create(:folder, user: authenticator.user) }
+    let!(:folder_membership) { FolderMembership.create(folder: folder, sign: sign) }
+
+    # We have added records so need to reload
+    before { visit topic_path(sign.topic) }
+
+    it "shows existing folder state" do
+      inside_card do
+        click_on "Folders"
+        active_checkbox = page.find_field("membership_folder_#{folder.id}_sign_#{sign.id}")
+        inactive_checkbox = page.find_field("membership_folder_#{other_folder.id}_sign_#{sign.id}")
+        expect(active_checkbox).to be_checked
+        expect(inactive_checkbox).not_to be_checked
+      end
+    end
+
+    it "adds the sign to a folder" do
+      inside_card do
+        click_on "Folders"
+        expect do
+          page.find("label", text: other_folder.title).click
+          wait_for_ajax
+        end.to change(FolderMembership, :count).by(1)
+      end
+    end
+
+    it "removes the sign from a folder" do
+      inside_card do
+        click_on "Folders"
+        expect do
+          page.find("label", text: folder.title).click
+          wait_for_ajax
+        end.to change(FolderMembership, :count).by(-1)
+      end
+    end
+
+    it "does not show the folder icon", signed_out: true do
+      expect(page).not_to have_selector ".sign-card__folders"
+    end
   end
 
   describe "Adding & removing from folders without JS" do
