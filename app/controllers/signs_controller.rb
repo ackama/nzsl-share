7f3e90cab9ca4ea5d6d4ee2022dc90082b3f77dd
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-10 16:36:36 +1300

    Add new signs controller actions for contributing a new sign

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -7,4 +7,30 @@ class SignsController < ApplicationController
 
     render
   end
+
+  def new
+    @sign = policy_scope(Sign).build
+    authorize @sign
+  end
+
+  def create
+    @sign = policy_scope(Sign).build(create_sign_params)
+    authorize @sign
+    return render(:new) unless @sign.save
+
+    flash[:notice] = t(".success")
+    respond_to do |format|
+      format.html { redirect_to @sign }
+      format.js { render inline: "window.location = '#{sign_path(@sign)}'" }
+    end
+  end
+
+  private
+
+  def create_sign_params
+    params
+      .require(:sign)
+      .permit(:video, :english)
+      .merge(contributor: current_user)
+  end
 end
--- app/policies/sign_policy.rb
+++ app/policies/sign_policy.rb
@@ -1,2 +1,15 @@
 class SignPolicy < ApplicationPolicy
+  def create?
+    contributor?
+  end
+
+  def new?
+    create?
+  end
+
+  private
+
+  def contributor?
+    user.present?
+  end
 end
