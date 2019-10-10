fbf6ab39250f3412de1c232dcf1b5ab3f8949bcc
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-11 11:02:43 +1300

    Rename drop-target component since it will also handle direct file lifecycle

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -14,6 +14,7 @@ import Rails from "@rails/ujs";
 Rails.start();
 import "foundation";
 import "../application.scss";
+import "../components/file-upload";
 import "../components/header";
 import "../components/modal";
 import "../components/character-count";
