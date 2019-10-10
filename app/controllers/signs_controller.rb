d2ca1f39afc02203a0c315fb9c245252ea2188e4
Laura Suzuki <lehsuzuki@gmail.com>
2019-10-11 11:48:49 +1300

    Remove policy scoping from show action - we want the user to be able to view any sign, not just their own

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -1,7 +1,7 @@
 class SignsController < ApplicationController
   def show
-    @sign = policy_scope(Sign.includes(:contributor, :topic))
-            .find(params[:id])
+    @sign = Sign.includes(:contributor, :topic)
+                .find(params[:id])
     authorize @sign
     return unless stale?(@sign)
 
