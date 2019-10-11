926fa649bf43cc50bf7905e78b9d3a9ccb49e679
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-11 15:31:03 +1300

    Move Sign restrictions to constants for sharing
    We'll have to think about how to handle this long-term - direct uploads,
    which use these rules, are not necessarily just for signs.

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Sign < ApplicationRecord
+  PERMITTED_CONTENT_TYPE_REGEXP = %r{\Avideo/.+\Z}.freeze
+  MAXIMUM_VIDEO_FILE_SIZE = 250.megabytes
+
   belongs_to :contributor, class_name: :User
   belongs_to :topic, optional: true
   has_one_attached :video
@@ -10,8 +13,8 @@ class Sign < ApplicationRecord
   # See app/validators/README.md for details on these
   # validations
   validates :video, attached: true,
-                    content_type: { with: %r{\Avideo/.+\Z} },
-                    size: { less_than: 250.megabytes }
+                    content_type: { with: PERMITTED_CONTENT_TYPE_REGEXP },
+                    size: { less_than: MAXIMUM_VIDEO_FILE_SIZE }
 
   # For now, this just returns the first 4 signs
   # It is defined here so the concept of a sign preview
