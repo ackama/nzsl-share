23481c6ab86152cdee315ed25094249789315970
Trevor Hose <trevor.hose@ackama.com>
2019-10-15 05:32:44 +1300

    Add share controller

---
+++ app/controllers/share_controller.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class ShareController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    with_exception_handling do
+      @folder = fetch_folder
+      authorize @folder
+      @folder.update(share_token: SecureRandom.uuid)
+      redirect_to_folders
+    end
+  end
+
+  def destroy
+    with_exception_handling do
+      @folder = fetch_folder_by_token
+      authorize @folder
+      @folder.update(share_token: nil)
+      redirect_to_folders
+    end
+  end
+
+  def show
+    with_exception_handling do
+      @folder = fetch_folder_by_token
+      authorize @folder
+      render :show
+    end
+  end
+
+  private
+
+  def with_exception_handling
+    yield if block_given?
+  rescue StandardError => e
+    logger.error { "Intercepted : #{e.inspect}" }
+    redirect_to_folders
+  end
+
+  def fetch_folder
+    policy_scope(Folder).find_by!(id: folder_id)
+  end
+
+  def fetch_folder_by_token
+    policy_scope(Folder).find_by!(id: folder_id, share_token: share_token)
+  end
+
+  def redirect_to_folders
+    redirect_to folders_path
+  end
+
+  def folder_id
+    params[:folder_id]
+  end
+
+  def share_token
+    params[:id]
+  end
+end
