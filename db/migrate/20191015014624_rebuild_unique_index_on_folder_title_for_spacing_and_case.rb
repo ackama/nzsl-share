04d35da644b07d2a878f19bbd9cf68034c29aa73
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-15 15:13:14 +1300

    Remove commented out code

--- db/migrate/20191015014624_rebuild_unique_index_on_folder_title_for_spacing_and_case.rb
+++ db/migrate/20191015014624_rebuild_unique_index_on_folder_title_for_spacing_and_case.rb
@@ -1,8 +1,5 @@
 class RebuildUniqueIndexOnFolderTitleForSpacingAndCase < ActiveRecord::Migration[6.0]
   def change
-    # execute <<~SQL
-    #   ADD UNIQUE INDEX folders_title_unique_idx ON folders (TRIM(BOTH FROM LOWER(title)))
-    # SQL
     add_index :folders,
               "user_id, TRIM(BOTH FROM LOWER(title))",
               unique: true,
