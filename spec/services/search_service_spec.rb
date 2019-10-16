bfa2f1ada96a91d11f857884bfffe87a96d4711f
Trevor Hose <trevor.hose@ackama.com>
2019-10-16 19:59:39 +1300

    Reorganise spec search data files
    * add search specs for total
    * separate refined data into own files
    * remove the now redundant refined data search.rb

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -1,11 +1,14 @@
 require "rails_helper"
-require "./spec/support/refined_data/search"
+
+require "./spec/support/refined_data/search/signs"
+require "./spec/support/refined_data/search/signs_with_chocolate"
+require "./spec/support/refined_data/search/signs_with_macrons"
 
 RSpec.describe SearchService, type: :service do
   describe "search" do
     context "english and secondary" do
       before(:each) do
-        Refined::Search::Data.signs.each do |sign_attrs|
+        Refined::Search::Signs.default.each do |sign_attrs|
           FactoryBot.create(:sign, sign_attrs)
         end
       end
@@ -61,7 +64,7 @@ RSpec.describe SearchService, type: :service do
 
     context "macrons" do
       before(:each) do
-        Refined::Search::Data.signs_with_macrons.each do |sign_attrs|
+        Refined::Search::Signs.with_macrons.each do |sign_attrs|
           FactoryBot.create(:sign, sign_attrs)
         end
       end
@@ -81,9 +84,85 @@ RSpec.describe SearchService, type: :service do
       end
     end
 
+    context "total" do
+      context "less than 10 results" do
+        before(:each) do
+          Refined::Search::Signs.default.each do |sign_attrs|
+            FactoryBot.create(:sign, sign_attrs)
+          end
+        end
+
+        it "returns result(s) total" do
+          rs1 = SearchService.call(search: Search.new(word: "a"))
+          expect(rs1.data.count).to eq rs1.support[:total]
+
+          rs2 = SearchService.call(search: Search.new(word: "ap"))
+          expect(rs2.data.count).to eq rs2.support[:total]
+
+          rs3 = SearchService.call(search: Search.new(word: "app"))
+          expect(rs3.data.count).to eq rs3.support[:total]
+
+          rs4 = SearchService.call(search: Search.new(word: "p"))
+          expect(rs4.data.count).to eq rs4.support[:total]
+
+          rs5 = SearchService.call(search: Search.new(word: "pp"))
+          expect(rs5.data.count).to eq rs5.support[:total]
+
+          rs6 = SearchService.call(search: Search.new(word: "l"))
+          expect(rs6.data.count).to eq rs6.support[:total]
+
+          rs7 = SearchService.call(search: Search.new(word: "le"))
+          expect(rs7.data.count).to eq rs7.support[:total]
+
+          rs8 = SearchService.call(search: Search.new(word: "e"))
+          expect(rs8.data.count).to eq rs8.support[:total]
+
+          rs9 = SearchService.call(search: Search.new(word: "123"))
+          expect(rs9.data.count).to eq rs9.support[:total]
+
+          rs10 = SearchService.call(search: Search.new(word: "#$%"))
+          expect(rs10.data.count).to eq rs10.support[:total]
+        end
+      end
+
+      context "more than 10 results" do
+        before(:each) do
+          Refined::Search::Signs.with_chocolate.each do |sign_attrs|
+            FactoryBot.create(:sign, sign_attrs)
+          end
+        end
+
+        it "returns page default limit and result(s) total" do
+          rs1 = SearchService.call(search: Search.new(word: "ch"))
+          expect(rs1.data.count).to eq Search::DEFAULT_LIMIT
+          expect(rs1.support[:total]).to eq 20
+
+          rs2 = SearchService.call(search: Search.new(word: "oco"))
+          expect(rs2.data.count).to eq Search::DEFAULT_LIMIT
+          expect(rs2.support[:total]).to eq 20
+
+          rs3 = SearchService.call(search: Search.new(word: "choc"))
+          expect(rs3.data.count).to eq Search::DEFAULT_LIMIT
+          expect(rs3.support[:total]).to eq 20
+
+          rs4 = SearchService.call(search: Search.new(word: "a"))
+          expect(rs4.data.count).to eq Search::DEFAULT_LIMIT
+          expect(rs4.support[:total]).to eq 20
+
+          rs5 = SearchService.call(search: Search.new(word: "chocolate"))
+          expect(rs5.data.count).to eq Search::DEFAULT_LIMIT
+          expect(rs5.support[:total]).to eq 20
+
+          rs6 = SearchService.call(search: Search.new(word: "ate"))
+          expect(rs6.data.count).to eq Search::DEFAULT_LIMIT
+          expect(rs6.support[:total]).to eq 20
+        end
+      end
+    end
+
     context "published date" do
       before(:each) do
-        Refined::Search::Data.signs.each do |sign_attrs|
+        Refined::Search::Signs.default.each do |sign_attrs|
           FactoryBot.create(:sign, :published, sign_attrs)
         end
       end
