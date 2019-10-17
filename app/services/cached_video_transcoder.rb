bfb942107520add9b0fd40a2f027a0494d6d2378
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-18 10:30:46 +1300

    Add services to encode video, and a wrapper to encode and cache video

---
+++ app/services/cached_video_transcoder.rb
@@ -0,0 +1,69 @@
+class CachedVideoTranscoder
+  delegate :service, to: :@blob
+
+  def initialize(blob, options, processor: default_processor)
+    @blob = blob
+    @options = options
+    @processor = processor.new(@options)
+  end
+
+  def exist?
+    service.exist?(key)
+  end
+
+  def processed
+    ensure_active_storage_host
+    return process unless processed?
+
+    encoded_blob.service_url
+  end
+
+  private
+
+  def processed?
+    encoded_blob && service.exist?(encoded_blob.key)
+  end
+
+  def encoded_blob
+    @encoded_blob ||= ActiveStorage::Blob.find_by(key: key)
+  end
+
+  def ensure_active_storage_host
+    ActiveStorage::Current.host ||= Rails.application.routes.default_url_options[:host]
+  end
+
+  def process
+    service.purge(key) if processed?
+
+    @processor.transcode(@blob) do |file|
+      service.upload(key, file[:io], **file)
+      @encoded_blob = persist_blob(file)
+      processed
+    end
+  end
+
+  def persist_blob(file)
+    file[:io].rewind
+    ActiveStorage::Blob
+      .new(file.except(:io))
+      .tap { |b| b.unfurl(file[:io]) }
+      .tap { |b| b.key = key }
+      .tap(&:save!)
+  end
+
+  # Use the same logic as the variation to determine the transcoded blob key
+  # If the blob being transcoded is itself a variant, then remove the encoder options
+  def key
+    parts = @blob.key
+                 .split("/", 3)
+                 .take(2)
+                 .reject { |str| str == "variants" }
+                 .unshift("variants")
+
+    "#{parts.join("/")}/#{Digest::SHA256.hexdigest(ActiveStorage::Variation.encode(@options))}"
+  end
+
+  def default_processor
+    VideoTranscoder
+  end
+end
