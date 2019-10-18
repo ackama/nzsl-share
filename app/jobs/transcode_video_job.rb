761c03bcc9bf689c00690955b51e10ef709f9378
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-18 15:28:04 +1300

    Discard transcode video jobs that fail because the blob already exists

--- app/jobs/transcode_video_job.rb
+++ app/jobs/transcode_video_job.rb
@@ -1,6 +1,10 @@
 class TranscodeVideoJob < ApplicationJob
   queue_as :video_encoding
 
+  # If this job runs and we run into this error, this just means that the blob
+  # has already been processed, so we don't need to retry.
+  discard_on ActiveRecord::RecordNotUnique
+
   def perform(blob, ffmpeg_args)
     CachedVideoTranscoder.new(blob, ffmpeg_args).processed
   end
