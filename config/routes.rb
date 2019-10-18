88292f80b620f076028df44816966ec42a2be89f
Laura Suzuki <laura@rabidtech.co.nz>
2019-10-18 13:11:34 +1300

    Add edit route to signs

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root "home#index"
   resources :search, only: [:index]
-  resources :signs, only: %i[show new create]
+  resources :signs, only: %i[show new create edit update]
   resources :topics, only: %i[index show]
 
   resources :folders do
