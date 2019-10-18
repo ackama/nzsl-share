4a2e50b0fcf9c86ce5ccedbfc5a216ebacca8893
Trevor Hose <trevor.hose@ackama.com>
2019-10-18 14:44:23 +1300

    Update search helper to show asc or desc

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,28 +1,41 @@
 module SearchHelper
-  def default_params(page)
-    {
-      page: page[:next_page],
-      word: page[:word],
-      total: page[:total]
-    }
+  def show_more_params(page)
+    next_page_params(page, english: "ASC")
+  end
+
+  def show_asc_params(page)
+    current_page_params(page, english: "ASC")
+  end
+
+  def show_desc_params(page)
+    current_page_params(page, english: "DESC")
   end
 
   def published_asc_params(page)
-    published_params(page, "ASC")
+    current_page_params(page, published: "ASC")
   end
 
   def published_desc_params(page)
-    published_params(page, "DESC")
+    current_page_params(page, published: "DESC")
   end
 
   private
 
-  def published_params(page, direction)
+  def current_page_params(page, order)
     {
       page: page[:current_page],
       word: page[:word],
       total: page[:total],
-      order: { published: direction }
+      order: order
+    }
+  end
+
+  def next_page_params(page, order)
+    {
+      page: page[:next_page],
+      word: page[:word],
+      total: page[:total],
+      order: order
     }
   end
 end
