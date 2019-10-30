b2da72dec50bdf0b3567dfe5e45e33f4bb8b4d90
Josh McArthur <joshua.mcarthur@gmail.com>
2019-10-31 11:29:20 +1300

    Check in schema.rb formatting change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017223900) do
+ActiveRecord::Schema.define(version: 2019_10_21_091806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
