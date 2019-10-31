f86e7a80d75a86e88591d28a5e299f841b901497
Trevor Hose <trevor.hose@ackama.com>
2019-11-01 07:46:40 +1300

    Sign share gets its own controller

---
+++ app/controllers/sign_share_controller.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class SignShareController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @sign = fetch_sign
+    authorize @sign
+    @sign.update(share_token: SecureRandom.uuid)
+    flash[:notice] = t(".success", share_url: share_url)
+    redirect_to_signs
+  end
+
+  def destroy
+    @sign = fetch_sign_by_token
+    authorize @sign
+    @sign.update(share_token: nil)
+    flash[:notice] = t(".success")
+    redirect_to_signs
+  end
+
+  def show
+    @sign = fetch_sign_by_token
+    authorize @sign
+    render "signs/show"
+  end
+
+  private
+
+  def share_url
+    "#{request.original_url}/#{@sign.share_token}"
+  end
+
+  def fetch_sign
+    policy_scope(Sign).find_by!(id: sign_id)
+  end
+
+  def fetch_sign_by_token
+    policy_scope(Sign).find_by!(id: sign_id, share_token: share_token)
+  end
+
+  def redirect_to_signs
+    redirect_to "#{signs_path}/#{sign_id}"
+  end
+
+  def sign_id
+    params[:sign_id]
+  end
+
+  def share_token
+    params[:id]
+  end
+end
