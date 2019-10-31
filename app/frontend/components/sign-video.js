98fc1d81ef214bb04b663638822c510b158a8c4c
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-01 11:09:32 +1300

    Allow video play/pause to be toggled with a click

---
+++ app/frontend/components/sign-video.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  // Hide sign controls on Firefox and other browsers not implementing
+  // pseodoclasses for controls
+  $(".sign-video").attr("controls", false);
+  $("<i class=\"sign-video__overlay\" alt=\"Play video\"></i>").insertAfter(".sign-video");
+
+  const togglePlayPause = (el) => {
+    el.classList.toggle("sign-video--playing");
+    el.paused ? el.play() : el.pause();
+
+    return false;
+  };
+
+  $(document).on("click", ".sign-video", e => togglePlayPause(e.target));
+  $(document).on("click", ".sign-video__overlay", e => togglePlayPause(e.target.previousElementSibling));
+});
+
