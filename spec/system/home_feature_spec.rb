89b764ac8a41aa29b11a072e6f114bc95f9e8835
Laura Suzuki <laura@rabidtech.co.nz>
2019-11-08 16:25:51 +1300

    Add system specs for home page

--- spec/system/home_feature_spec.rb
+++ spec/system/home_feature_spec.rb
@@ -1,11 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "Homepage", type: :system do
-  before { visit root_path }
+  before do
+    FactoryBot.create_list(:sign, 10)
+    visit root_path
+  end
 
   it "rendered page contains both base and application layouts" do
     assert_selector("html>head+body")
     assert_selector("body p")
     assert_match(/Home/, page.title)
+  end
+
+  it "header search nav is not visible initially", uses_javascript: true do
+    expect(page).to have_css("#header-nav", visible: false)
+  end
+
+  it "header search shows and hides on scroll", uses_javascript: true do
+    signs = page.find(".copy__signs", match: :first)
+    banner = page.find(".banner")
+    execute_script("arguments[0].scrollIntoView();", signs)
+    expect(page).to have_css("#header-nav", visible: true)
+    execute_script("arguments[0].scrollIntoView();", banner)
+    expect(page).to have_css("#header-nav", visible: false)
+  end
+
+  it "displays recently added and most viewed signs" do
+    expect(page).to have_css(".sign-card", count: 8)
   end
 end
