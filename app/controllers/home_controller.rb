120b5d1fec2979bd2654311be2260aa648b2cda8
Laura Suzuki <laura@rabidtech.co.nz>
2019-11-08 13:21:22 +1300

    Implement home and authenticated home layouts so that the banner is outside the grid-container Render these in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 # Renders the home page.
 class HomeController < ApplicationController
+  layout -> { user_signed_in? ? "authenticated_home" : "home" }
+
   def index
-    @signs = policy_scope(Sign).first(4)
+    @recent_signs = policy_scope(Sign).first(4)
+    @viewed_signs = policy_scope(Sign).last(4)
   end
 end
