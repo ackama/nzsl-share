d081f0e03a10e9a3f091a2d7b94e069542fb21a0
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-08 13:35:58 +1300

    Updates to contribute sign spec to cover new behaviour

--- spec/system/contribute_sign_feature_spec.rb
+++ spec/system/contribute_sign_feature_spec.rb
@@ -12,9 +12,10 @@ RSpec.describe "Contributing a new sign", type: :system do
       end.to change(subject.user.signs, :count).by(1)
     end
 
-    it "using drag and drop can contribute a valid video file", uses_javascript: true do
+    it "using drag and drop can contribute a valid video file" do
       expect do
         subject.drop_file_in_file_upload
+        subject.submit
       end.to change(subject.user.signs, :count).by(1)
     end
 
@@ -28,17 +29,14 @@ RSpec.describe "Contributing a new sign", type: :system do
     it "is prevented from contributing an invalid file" do |example|
       subject.choose_file(Rails.root.join("spec", "fixtures", "dummy.exe"))
 
-      # Until uploading JS is implemented, an alert is opened on direct upload error
+      # Until uploading JS is implemented, an error is shown using the raw direct upload error
       # from rails-ujs. This means that the test (temporarily) needs to expect to see an
-      # alert rather than page content when running in JS mode.
+      # technical rather than useful error message when running in JS mode.
       if example.metadata[:uses_javascript]
-        sleep 0.5 # Wait for the alert to be visible
-        alert = page.driver.browser.switch_to.alert
-        expect(alert.text).to eq "Error creating Blob for \"dummy.exe\". Status: 422"
-        alert.accept
+        expect(subject).to have_error "Error creating Blob for \"dummy.exe\". Status: 422"
       else
         subject.click_on("Start Upload")
-        expect(subject).to have_error "Video isn't a valid video file"
+        expect(subject).to have_error "Video file is not of an accepted type"
       end
     end
   end
