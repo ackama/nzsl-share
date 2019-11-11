282c8c4a7b9b99a254f392b54e9ca270c99547da
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-12 09:23:24 +1300

    Add counter_cache on user folders
    This makes it more efficient to query for the number of folders a user has.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_043432) do
+ActiveRecord::Schema.define(version: 2019_11_11_201318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,9 +82,9 @@ ActiveRecord::Schema.define(version: 2019_11_06_043432) do
     t.bigint "contributor_id", null: false
     t.bigint "topic_id"
     t.text "description"
-    t.text "notes"
     t.boolean "processed_videos", default: false, null: false
     t.boolean "processed_thumbnails", default: false, null: false
+    t.text "notes"
     t.string "share_token"
     t.string "status", null: false
     t.datetime "submitted_at"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_043432) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "folders_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
