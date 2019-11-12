ae1798754b06b2dacef484e114d1d47b0c973fb3
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-12 14:16:58 +1300

    Tidy up routes since all can now use sign attachment routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,7 @@ Rails.application.routes.draw do
   resources :signs, except: %i[index] do
     resources :videos, param: :preset, only: :show, controller: :sign_video
     resources :share, only: %i[show create destroy], controller: :sign_share, param: :token
-    resources :usage_examples, only: %i[destroy], controller: :sign_attachments
-    resources :illustrations, only: %i[destroy], controller: :sign_attachments
-    resources :sign_attachments, only: %i[create],
+    resources :sign_attachments, only: %i[create update destroy],
                                  path: "/:attachment_type",
                                  as: :attachments,
                                  constraints: { attachment_type: /usage_examples|illustrations/ }
