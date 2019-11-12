eb88507202add86d43f0be0182fefd6e48ea9bac
Mischa Saunders <mischa.saunders@gmail.com>
2019-11-13 11:05:05 +1300

    update mysigns method to simpler definition

--- app/controllers/sign_publish_controller.rb
+++ app/controllers/sign_publish_controller.rb
@@ -24,7 +24,7 @@ class SignPublishController < ApplicationController
   private
 
   def my_signs
-    @signs = policy_scope(Sign.where(contributor: current_user)).order(word: :asc)
+    @signs.where(contributor: current_user)
   end
 
   def redirect_after_update(sign)
--- app/controllers/sign_request_publish_controller.rb
+++ app/controllers/sign_request_publish_controller.rb
@@ -14,7 +14,7 @@ class SignRequestPublishController < ApplicationController
   private
 
   def my_signs
-    @signs = policy_scope(Sign.where(contributor: current_user)).order(word: :asc)
+    @signs.where(contributor: current_user)
   end
 
   def redirect_after_update(sign)
