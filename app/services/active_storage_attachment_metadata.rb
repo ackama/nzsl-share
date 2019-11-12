1553c6b9056ebf7df8da18177ff7840c0a374e05
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-12 14:16:23 +1300

    Add service to abstract management of ActiveStorage metadata data

---
+++ app/services/active_storage_attachment_metadata.rb
@@ -0,0 +1,29 @@
+class ActiveStorageAttachmentMetadata
+  delegate :metadata, to: :@blob
+
+  def initialize(attachment)
+    @blob = attachment.blob
+  end
+
+  def set(key, value)
+    metadata.merge!(key => value)
+  end
+
+  def set!(key, value)
+    set(key, value).tap { save! }
+  end
+
+  def get(key)
+    metadata.fetch(key, nil)
+  end
+
+  def all
+    metadata.dup
+  end
+
+  private
+
+  def save!
+    @blob.update!(metadata: metadata)
+  end
+end
