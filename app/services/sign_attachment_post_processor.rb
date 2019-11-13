2685e32ceffeb252f06b2e818f0ec5016f1744b4
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-13 14:07:08 +1300

    Refactor post processing to support sign videos and sign attachments

---
+++ app/services/sign_attachment_post_processor.rb
@@ -0,0 +1,57 @@
+class SignAttachmentPostProcessor
+  def initialize(blob, presets=nil)
+    @blob = blob
+    @presets = presets || default_presets
+  end
+
+  def process
+    [
+      batch("Generate thumbnails") { thumbnail_processes },
+      (@blob.video? ? batch("Transcode videos") { video_processes } : nil)
+    ]
+  end
+
+  private
+
+  def video_processes
+    @presets[:video].each { |preset| TranscodeVideoJob.perform_unique_later(@blob, preset.to_a) }
+  end
+
+  def thumbnail_processes
+    @presets[:thumbnail].each { |preset| GenerateThumbnailJob.perform_later(@blob, preset.to_h) }
+  end
+
+  def batch(description, callback_handler=nil, callback_data={}, &block)
+    batch = new_batch
+    batch.description = "Post processing: #{description} for Blob ##{@blob.id}"
+
+    # We intentionally don't monitor completion, since that can be triggered
+    # for incomplete runs. Instead, we just listen for success and allow failed
+    # batches to run through the failed/retry/dead Sidekiq workflow.
+    batch.on(:success, callback_handler, callback_data) if callback_handler
+
+    batch.jobs(&block)
+    Rails.logger.info "Started batch: #{batch.bid} - #{batch.description}"
+
+    batch
+  end
+
+  def new_batch
+    Sidekiq::Batch.new
+  end
+
+  def default_presets
+    {
+      video: [
+        VideoEncodingPreset.default.scale_1080.muted,
+        VideoEncodingPreset.default.scale_720.muted,
+        VideoEncodingPreset.default.scale_360.muted
+      ],
+      thumbnail: [
+        ThumbnailPreset.default.scale_1080,
+        ThumbnailPreset.default.scale_720,
+        ThumbnailPreset.default.scale_360
+      ]
+    }
+  end
+end
--- app/services/sign_post_processor.rb
+++ app/services/sign_post_processor.rb
@@ -1,4 +1,4 @@
-class SignPostProcessor
+class SignPostProcessor < SignAttachmentPostProcessor
   class ThumbnailCallback
     def on_success(_status, options)
       sign = Sign.find(options.fetch("sign_id"))
@@ -13,63 +13,21 @@ class SignPostProcessor
     end
   end
 
-  def initialize(sign, presets=default_presets)
+  def initialize(sign, presets=nil)
     @sign = sign
-    @presets = presets
+    super(@sign.video.blob, presets)
   end
 
   def process
     sign.update!(processed_videos: false, processed_thumbnails: false)
 
     [
-      batch("Generate thumbnails", ThumbnailCallback) { thumbnail_processes },
-      batch("Transcode videos", VideoCallback) { video_processes }
+      batch("Generate thumbnails", ThumbnailCallback, sign_id: sign.id) { thumbnail_processes },
+      batch("Transcode videos", VideoCallback, sign_id: sign.id) { video_processes }
     ]
   end
 
   private
-
-  def video_processes
-    @presets[:video].each { |preset| TranscodeVideoJob.perform_unique_later(sign.video.blob, preset.to_a) }
-  end
-
-  def thumbnail_processes
-    @presets[:thumbnail].each { |preset| GenerateThumbnailJob.perform_later(sign.video.blob, preset.to_h) }
-  end
-
-  def batch(description, callback_handler, &block)
-    batch = new_batch
-    batch.description = "Post processing: #{description} for Sign ##{sign.id}"
-
-    # We intentionally don't monitor completion, since that can be triggered
-    # for incomplete runs. Instead, we just listen for success and allow failed
-    # batches to run through the failed/retry/dead Sidekiq workflow.
-    batch.on(:success, callback_handler, sign_id: sign.id)
-
-    batch.jobs(&block)
-    Rails.logger.info "Started batch: #{batch.bid} - #{batch.description}"
-
-    batch
-  end
-
-  def new_batch
-    Sidekiq::Batch.new
-  end
-
-  def default_presets
-    {
-      video: [
-        VideoEncodingPreset.default.scale_1080.muted,
-        VideoEncodingPreset.default.scale_720.muted,
-        VideoEncodingPreset.default.scale_360.muted
-      ],
-      thumbnail: [
-        ThumbnailPreset.default.scale_1080,
-        ThumbnailPreset.default.scale_720,
-        ThumbnailPreset.default.scale_360
-      ]
-    }
-  end
 
   attr_reader :sign
 end
