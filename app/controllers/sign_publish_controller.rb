f80495b768ca3c34f00deb193a15d3884dd744e7
Mischa Saunders <mischa.saunders@gmail.com>
2019-11-14 09:49:31 +1300

    dry up code in request publish and publish controllers

--- app/controllers/sign_publish_controller.rb
+++ app/controllers/sign_publish_controller.rb
@@ -2,39 +2,26 @@ class SignPublishController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @sign = my_signs.find(params["sign_id"])
+    @sign = signs.find(params["sign_id"])
     @sign.publish
     authorize @sign, :publish?
     @sign.save
 
-    flash[:notice] = t(".success")
-    redirect_after_update(@sign)
+    redirect_back(fallback_location: @sign, notice: t(".success"))
   end
 
   def destroy
-    @sign = my_signs.find(params["sign_id"])
+    @sign = signs.find(params["sign_id"])
     @sign.set_sign_to_personal
     authorize @sign, :unpublish?
     @sign.save
 
-    flash[:notice] = t(".success")
-    redirect_after_update(@sign)
+    redirect_back(fallback_location: @sign, notice: t(".success"))
   end
 
   private
 
   def signs
     policy_scope(Sign).order(word: :asc)
-  end
-
-  def my_signs
-    signs.where(contributor: current_user)
-  end
-
-  def redirect_after_update(sign)
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: sign) }
-      format.js { render inline: "window.location = '#{sign_path(sign)}'" }
-    end
   end
 end
--- app/controllers/sign_request_publish_controller.rb
+++ app/controllers/sign_request_publish_controller.rb
@@ -2,29 +2,17 @@ class SignRequestPublishController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    @sign = my_signs.find(params["sign_id"])
+    @sign = signs.find(params["sign_id"])
     @sign.request_unpublish
     authorize @sign, :request_unpublish?
     @sign.save
 
-    flash[:notice] = t(".success")
-    redirect_after_update(@sign)
+    redirect_back(fallback_location: @sign, notice: t(".success"))
   end
 
   private
 
   def signs
     policy_scope(Sign).order(word: :asc)
-  end
-
-  def my_signs
-    signs.where(contributor: current_user)
-  end
-
-  def redirect_after_update(sign)
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: sign) }
-      format.js { render inline: "window.location = '#{sign_path(sign)}'" }
-    end
   end
 end
