9de85fa3b5d9cd29550aeefda8cc2d8a5c3c878f
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-15 18:29:32 +1300

    Allow administration actions to be toggled on and off for shared example tests

--- spec/support/shared_examples/administrate_shared_examples.rb
+++ spec/support/shared_examples/administrate_shared_examples.rb
@@ -1,24 +1,39 @@
-RSpec.shared_examples "an Administrate dashboard" do |url_base|
+RSpec.shared_examples "an Administrate dashboard" do |url_base, only: %i[index show new edit destroy], except: []|
   include AdministratePageHelpers
+  actions = only - except
 
   before { visit_admin url_base }
 
-  it "can visit the index page" do
-    expect(page).to have_selector("tr.js-table-row")
+  if actions.include?(:index)
+    it "can visit the index page" do
+      expect(page).to have_selector("tr.js-table-row")
+    end
   end
 
-  it "can visit the show page" do
-    click_on_first_row
-    expect(header).to start_with "Show"
+  if actions.include?(:new)
+    it "can visit the new page" do
+      expect(page).to click_on "New #{url_base.to_s.titleize}"
+    end
   end
 
-  it "can visit the edit page" do
-    within(first_row) { click_on "Edit" }
-    expect(header).to start_with "Edit"
+  if actions.include?(:show)
+    it "can visit the show page" do
+      click_on_first_row
+      expect(header).to start_with "Show"
+    end
   end
 
-  it "can destroy a record" do
-    within(mid_row) { click_on "Destroy" }
-    expect(page).to have_content "was successfully destroyed."
+  if actions.include?(:edit)
+    it "can visit the edit page" do
+      within(first_row) { click_on "Edit" }
+      expect(header).to start_with "Edit"
+    end
+  end
+
+  if actions.include?(:destroy)
+    it "can destroy a record" do
+      within(mid_row) { click_on "Destroy" }
+      expect(page).to have_content "was successfully destroyed."
+    end
   end
 end
