35f805c5129ae51e69c670bec4db00a68edd5927
Trevor Hose <trevor.hose@ackama.com>
2019-11-19 03:48:26 +1300

    Add scope for share_policy defined by share_token
    * used by whoever has recieved a share_token including anonomouse
      users so scope should include data that has the share_token set

--- app/policies/share_policy.rb
+++ app/policies/share_policy.rb
@@ -4,4 +4,10 @@ class SharePolicy < ApplicationPolicy
   def show?
     record.fetch(:token) == record.fetch(:shared).share_token
   end
+
+  class Scope < Scope
+    def resolve
+      scope.where.not(share_token: nil)
+    end
+  end
 end
