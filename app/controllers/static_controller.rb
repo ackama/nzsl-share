c4cfc1c7452bb06d0f49690a6a59d91b1bb09690
Mischa Saunders <mischa.saunders@gmail.com>
2019-11-20 09:36:10 +1300

    update footer with latest links from client

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def show
-    fail ActionController::RoutingError unless "about contact".include? params[:page]
+    fail ActionController::RoutingError unless "about contact terms-and-conditions".include? params[:page]
 
     render params[:page]
   end
