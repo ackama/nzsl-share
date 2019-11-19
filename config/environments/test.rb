d34ddece0d34b1a4e77388442e0f0b4dfa1da2fc
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-20 12:40:18 +1300

    Run jobs asynchrously during tests to avoid intermittent failures
    When files are uploaded, they are processed. In particular, videos are
    encoded. We need to disable this, as it is leading to completely random
    test failures depending on whether the job finishes processing before
    the test times out.
    
    There may be some tests that require an inline adapter. In these
    scenarios, we should just set the adapter for that session.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,8 +50,6 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  # Run queued tasks inline
-  Rails.application.config.active_job.queue_adapter = :inline
-  require "sidekiq/testing"
-  Sidekiq::Testing.inline!
+  # Run queued tasks in an async queue
+  Rails.application.config.active_job.queue_adapter = :async
 end
