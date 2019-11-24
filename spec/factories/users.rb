71884f22b6f9d30f5551b520109000c9775c3e79
Laura Suzuki <laura@rabidtech.co.nz>
2019-11-25 12:14:00 +1300

    Make user bio default in factory, use faker to generate it

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "#{Faker::Internet.username}#{n}" }
     email { Faker::Internet.email }
+    bio { Faker::Lorem.paragraph_by_chars(256, false) }
     password { "password" }
     password_confirmation { "password" }
     confirmed_at { Time.zone.now }
@@ -12,10 +13,6 @@ FactoryBot.define do
 
     trait :administrator do
       administrator { true }
-    end
-
-    trait :with_bio do
-      bio { "I love NZSL!" }
     end
   end
 end
