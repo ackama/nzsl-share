fa6edfb91cf84f6832a1d11ccea8707f83350d50
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-25 08:59:10 +1300

    Send a notification to the admins when an application is submitted.

--- spec/system/approved_user_application_spec.rb
+++ spec/system/approved_user_application_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "Approved user application", type: :system do
   include ActiveJob::TestHelper
   let(:user) { FactoryBot.create(:user) }
+  let!(:admin) { FactoryBot.create(:user, :administrator) } # We need a user to receive admin emails
   subject { ApprovedUserApplicationFeature.new }
   before { subject.start(user) }
 
@@ -19,6 +20,14 @@ RSpec.describe "Approved user application", type: :system do
     mail = ActionMailer::Base.deliveries[-2]
     expect(mail.to).to eq [user.email]
     expect(mail.subject).to eq I18n.t("approved_user_mailer.submitted.subject")
+  end
+
+  it "sends an email to the admin after submitting" do
+    subject.fill_in_mandatory_fields
+    perform_enqueued_jobs { subject.submit }
+    mail = ActionMailer::Base.deliveries[-1]
+    expect(mail.to).to eq [admin.email]
+    expect(mail.subject).to eq I18n.t("approved_user_mailer.admin_submitted.subject")
   end
 
   it "leaves a required field blank" do
