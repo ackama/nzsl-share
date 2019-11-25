1a69b47fc20a9ff2fe33b06b50f6b11be6ba3ebd
Trevor Hose <trevor.hose@ackama.com>
2019-11-25 14:08:05 +1300

    Move sign folder button behaviour to own helper
    * satisfy rubocop checks by making the signs_helper smaller by moving
      button behaviour into own helper method

--- app/helpers/signs_helper.rb
+++ app/helpers/signs_helper.rb
@@ -1,56 +1,4 @@
 module SignsHelper
-  ##
-  # Determine what kind of button to display to the user:
-  #
-  #  1. User is not signed. Render a link to the sign in page, that has all the necessary classes to
-  #     look like the 'add to folder' button.
-  #  2. Render the normal 'add to folder' button that opens a dropdown menu
-  #  3. Render a modifier 'add to folder' button that opens a dropdown menu but has a different visual
-  #     appearance to indicate the sign is already in the folder.
-  def folder_button(sign)
-    classes = %w[sign-card__folders__button]
-
-    return link_to_login(add_folder_icon, classes) unless user_signed_in?
-
-    in_folder = sign.available_folders.reject { |_folder, membership| membership.nil? }.any?
-    classes << "sign-card__folders__button--in-folder" if in_folder
-    button_tag(
-      in_folder ? in_folder_icon : add_folder_icon,
-      class: classes,
-      data: data(sign))
-  end
-
-  def sign_show_folder_button(sign)
-    classes = %w[button clear medium]
-
-    return link_to_login(sign_show_folder_icon, classes) unless user_signed_in?
-
-    button_tag(
-      sign_show_folder_icon,
-      class: classes,
-      data: data(sign)
-    )
-  end
-
-  def fetch_folder_button(sign)
-    return sign_show_folder_button(sign) if sign_show_folder_button?(sign)
-
-    folder_button(sign)
-  end
-
-  def sign_show_folder_button?(sign)
-    show_path = "/signs/#{sign.id}"
-
-    return true if request.path == show_path
-    return true if request.referer.include?(show_path) && request.path.include?("/folder_memberships")
-
-    false
-  end
-
-  def data(sign)
-    { toggle: dom_id(sign, :folder_menu) }
-  end
-
   def agree_button(sign, extra_classes="grid-x align-middle", &block)
     classes = "sign-card__votes--agree #{extra_classes}"
     return content_tag(:div, class: classes, &block) unless policy(sign).agree?
@@ -83,14 +31,6 @@ module SignsHelper
                                           data: { remote: true },
                                           title: "Disagree",
                                           class: classes, &block)
-  end
-
-  def in_folder_icon
-    inline_svg("media/images/folder-success.svg", title: "Folders", aria: true, class: "icon")
-  end
-
-  def add_folder_icon
-    inline_svg("media/images/folder-add.svg", title: "Folders", aria: true, class: "icon")
   end
 
   def overview_reject_link(sign)
@@ -145,13 +85,5 @@ module SignsHelper
 
   def approve_title(sign)
     sign.submitted? ? "Publish" : "Make Private"
-  end
-
-  def sign_show_folder_icon
-    inline_svg("media/images/folder-add.svg", aria: true, class: "icon icon--medium") + "Add to Folder"
-  end
-
-  def link_to_login(icon, classes)
-    link_to(icon, new_user_session_path, class: classes)
   end
 end
