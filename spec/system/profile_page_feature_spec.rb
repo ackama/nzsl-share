6e960bcbb49842eaa1683853b14b4f6ae1ec4d18
Laura Suzuki <laura@rabidtech.co.nz>
2019-11-25 14:44:42 +1300

    Add editing specs to profile page feature specs

--- spec/system/profile_page_feature_spec.rb
+++ spec/system/profile_page_feature_spec.rb
@@ -2,24 +2,56 @@ require "rails_helper"
 
 RSpec.describe "Profile page", type: :system do
   let!(:user) { FactoryBot.create(:user) }
+  let(:auth) { AuthenticateFeature.new(user) }
 
-  before { visit user_path(user) }
+  context "viewing" do
+    before { visit user_path(user) }
 
-  it "shows username" do
-    within ".form" do
-      expect(page).to have_content(user.username)
+    it "shows username" do
+      within ".form" do
+        expect(page).to have_content(user.username)
+      end
+    end
+
+    it "shows bio" do
+      within ".form" do
+        expect(page).to have_content(user.bio)
+      end
+    end
+
+    it "shows profile picture" do
+      within ".form" do
+        expect(page).to have_selector(".avatar")
+      end
     end
   end
 
-  it "shows bio" do
-    within ".form" do
-      expect(page).to have_content(user.bio)
-    end
-  end
+  context "editing" do
+    before { auth.sign_in }
 
-  it "shows profile picture" do
-    within ".form" do
-      expect(page).to have_selector(".avatar")
+    it "nav links to edit profile page", uses_javascript: true do
+      visit root_path
+      within ".navigation__item--account" do
+        click_on user.username
+      end
+      click_on "Edit my profile"
+      expect(page).to have_current_path("/users/edit")
+    end
+
+    it "can edit my details successfully" do
+      visit edit_user_registration_path(user)
+      expect(page).to have_content "Edit profile"
+      fill_in "Bio", with: "My new biography"
+      fill_in "Current password", with: user.password
+      click_on "Update"
+      expect(page).to have_content I18n.t("devise.registrations.updated")
+    end
+
+    it "displays errors when I don't confirm my password" do
+      visit edit_user_registration_path(user)
+      fill_in "Bio", with: "My new biography"
+      click_on "Update"
+      expect(page).to have_css ".invalid"
     end
   end
 end
