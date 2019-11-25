355c64cd32cfa53a577a1bf2c47fb79065d55763
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-26 09:08:09 +1300

    Revert "Tests in Chrome run faster in the foreground for some reason"
    No they don't.
    
    This reverts commit 4b7cea67938f4e035e7e452b53797c81ce3fbf9b.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,6 +46,7 @@ Capybara.register_driver :chrome do |app|
   options.add_argument("--no-proxy-server")
   options.add_argument("--proxy-server='direct://'")
   options.add_argument("--proxy-bypass-list=*")
+  options.add_argument("--headless") unless ENV["FOREGROUND"]
 
   Capybara::Selenium::Driver.new app, browser: :chrome, options: options
 end
