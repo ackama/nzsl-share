ed8cd6d0f3b50a143995cac18b17f375eaa253ba
Trevor Hose <trevor.hose@ackama.com>
2019-11-28 11:06:46 +1300

    Add nullify to has_many signs
    * this will destroy the topic and update the sign.topic_id to
      nil

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :signs, inverse_of: :topic, dependent: :destroy
+  has_many :signs, inverse_of: :topic, dependent: :nullify
   scope :featured, -> { where.not(featured_at: nil).order(featured_at: :desc) }
 
   def featured
