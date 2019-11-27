a9721921a568822f19f229b5de193c153d468a7f
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-28 09:29:36 +1300

    RoutingError expects a message argument

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   def show
     unless %w[about contact terms-and-conditions privacy-policy].include? params[:page]
+      fail ActionController::RoutingError, "Unknown page: #{params[:page]}"
     end
 
     render params[:page]
