419288109db85977753e306e24fd019477929bc9
Trevor Hose <trevor.hose@ackama.com>
2019-11-28 09:50:56 +1300

    Add feature accessor to model
    * and update the topic dashboard to use it

--- app/dashboards/topic_dashboard.rb
+++ app/dashboards/topic_dashboard.rb
@@ -10,7 +10,7 @@ class TopicDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     signs: Field::HasMany,
     name: Field::String,
-    featured_at: Field::DateTime
+    featured: Field::Boolean
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -19,16 +19,16 @@ class TopicDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    signs
     name
-    featured_at
+    featured
+    signs
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     name
-    featured_at
+    featured
   ].freeze
 
   # FORM_ATTRIBUTES
@@ -36,7 +36,7 @@ class TopicDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     name
-    featured_at
+    featured
   ].freeze
 
   # COLLECTION_FILTERS
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,12 @@ class Topic < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :signs, inverse_of: :topic, dependent: :destroy
   scope :featured, -> { where.not(featured_at: nil).order(featured_at: :desc) }
+
+  def featured
+    featured_at.present?
+  end
+
+  def featured=(value)
+    self.featured_at = value.to_i == 1 ? Time.zone.now : nil
+  end
 end
