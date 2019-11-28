9f90eb5b6e6973aabb9c2b87545bf14b0e76d250
Josh McArthur <joshua.mcarthur@gmail.com>
2019-11-28 13:11:08 +1300

    System user must have a valid username to be routable

--- app/models/system_user.rb
+++ app/models/system_user.rb
@@ -1,7 +1,7 @@
 class SystemUser
   class << self
     def find
-      User.where(username: "nzsl-share").first_or_initialize do |user|
+      User.where(username: "nzsl.share").first_or_initialize do |user|
         # We do not want the system user to be log-in-able.
         # This means no email or password.
         user.save(validate: false)
