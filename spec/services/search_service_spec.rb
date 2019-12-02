eca034221eaf7bf6ced94788b2285cf5c600b963
Josh McArthur <joshua.mcarthur@gmail.com>
2019-12-03 11:25:06 +1300

    Updates to SearchService spec to match FreelexSearchService spec

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -11,63 +11,67 @@ RSpec.describe SearchService, type: :service do
     context "primary word and secondary" do
       before(:each) do
         Refined::Search::Signs.default.each do |sign_attrs|
-          sign_attrs[:status] ||= "published"
-          sign_attrs[:conditions_accepted] ||= true
-          FactoryBot.create(:sign, sign_attrs)
+          FactoryBot.create(:sign, :published, sign_attrs)
         end
       end
 
       it "returns 0 results if policy scope is none" do
         expect(
-          SearchService.call(relation: Pundit.policy_scope(user, Sign.none), search: Search.new(term: "a")).data.count
+          search(Pundit.policy_scope(user, Sign.none), term: "a").data.count
         ).to eq 0
       end
 
+      it "does not include non-published results" do
+        expect do
+          FactoryBot.create(:sign, :submitted, Refined::Search::Signs.default.first)
+        end.not_to change(search(scoped_relation, term: "a").data, :count)
+      end
+
       it "returns result(s) given a term fragment" do
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "a")).data.count).to eq 7
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "ap")).data.count).to eq 2
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "app")).data.count).to eq 1
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "appl")).data.count).to eq 1
+        expect(search(scoped_relation, term: "a").data.count).to eq 7
+        expect(search(scoped_relation, term: "ap").data.count).to eq 2
+        expect(search(scoped_relation, term: "app").data.count).to eq 1
+        expect(search(scoped_relation, term: "appl").data.count).to eq 1
 
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "p")).data.count).to eq 6
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "pp")).data.count).to eq 1
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "ppl")).data.count).to eq 1
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "pple")).data.count).to eq 1
+        expect(search(scoped_relation, term: "p").data.count).to eq 6
+        expect(search(scoped_relation, term: "pp").data.count).to eq 1
+        expect(search(scoped_relation, term: "ppl").data.count).to eq 1
+        expect(search(scoped_relation, term: "pple").data.count).to eq 1
 
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "p")).data.count).to eq 6
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "pl")).data.count).to eq 1
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "ple")).data.count).to eq 1
+        expect(search(scoped_relation, term: "p").data.count).to eq 6
+        expect(search(scoped_relation, term: "pl").data.count).to eq 1
+        expect(search(scoped_relation, term: "ple").data.count).to eq 1
 
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "l")).data.count).to eq 7
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "le")).data.count).to eq 2
+        expect(search(scoped_relation, term: "l").data.count).to eq 7
+        expect(search(scoped_relation, term: "le").data.count).to eq 2
 
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "e")).data.count).to eq 7
+        expect(search(scoped_relation, term: "e").data.count).to eq 7
       end
 
       it "returns result(s) given a complete term" do
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "pie")).data.count).to eq 3
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "america")).data.count).to eq 3
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "risotto")).data.count).to eq 2
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "england")).data.count).to eq 2
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "soup")).data.count).to eq 2
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "cream")).data.count).to eq 2
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "apple")).data.count).to eq 1
+        expect(search(scoped_relation, term: "pie").data.count).to eq 3
+        expect(search(scoped_relation, term: "america").data.count).to eq 3
+        expect(search(scoped_relation, term: "risotto").data.count).to eq 2
+        expect(search(scoped_relation, term: "england").data.count).to eq 2
+        expect(search(scoped_relation, term: "soup").data.count).to eq 2
+        expect(search(scoped_relation, term: "cream").data.count).to eq 2
+        expect(search(scoped_relation, term: "apple").data.count).to eq 1
       end
 
       it "returns an exact match first given a complete term" do
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "apple"))
+        expect(search(scoped_relation, term: "apple")
            .data.pluck(:word, :secondary).flatten.to_s.include?("apple")).to be true
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "pie"))
+        expect(search(scoped_relation, term: "pie")
           .data.pluck(:word, :secondary).flatten.to_s.include?("pie")).to be true
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "banana"))
+        expect(search(scoped_relation, term: "banana")
           .data.pluck(:word, :secondary).flatten.to_s.include?("banana")).to be true
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "england"))
+        expect(search(scoped_relation, term: "england")
           .data.pluck(:word, :secondary).flatten.to_s.include?("england")).to be true
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "cream"))
+        expect(search(scoped_relation, term: "cream")
           .data.pluck(:word, :secondary).flatten.to_s.include?("cream")).to be true
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "risotto"))
+        expect(search(scoped_relation, term: "risotto")
           .data.pluck(:word, :secondary).flatten.to_s.include?("risotto")).to be true
-        expect(SearchService.call(relation: scoped_relation, search: Search.new(term: "blueberry"))
+        expect(search(scoped_relation, term: "blueberry")
           .data.pluck(:word, :secondary).flatten.to_s.include?("blueberry")).to be true
       end
     end
@@ -75,40 +79,37 @@ RSpec.describe SearchService, type: :service do
     context "whitespace" do
       before(:each) do
         Refined::Search::Signs.with_chocolate.each do |sign_attrs|
-          sign_attrs[:status] ||= "published"
-          sign_attrs[:conditions_accepted] = true
-
-          FactoryBot.create(:sign, sign_attrs)
+          FactoryBot.create(:sign, :published, sign_attrs)
         end
       end
 
       it "matches exact term" do
         t1 = "chocolate ice cream"
-        rs1 = SearchService.call(relation: scoped_relation, search: Search.new(term: t1))
+        rs1 = search(scoped_relation, term: t1)
         expect(rs1.data.count && rs1.support[:total]).to eq 1
         expect(rs1.data.pluck(:word).include?(t1)).to be true
 
         t2 = "chocolate fondant"
-        rs2 = SearchService.call(relation: scoped_relation, search: Search.new(term: t2))
+        rs2 = search(scoped_relation, term: t2)
         expect(rs2.data.count && rs2.support[:total]).to eq 1
         expect(rs2.data.pluck(:word).include?(t2)).to be true
 
         t3 = "chocolate souffle"
-        rs3 = SearchService.call(relation: scoped_relation, search: Search.new(term: t3))
+        rs3 = search(scoped_relation, term: t3)
         expect(rs3.data.count && rs3.support[:total]).to eq 1
         expect(rs3.data.pluck(:word).include?(t3)).to be true
       end
 
       it "matches partial term" do
         t1 = "chocolate f"
-        rs1 = SearchService.call(relation: scoped_relation, search: Search.new(term: t1))
+        rs1 = search(scoped_relation, term: t1)
         expect(rs1.data.count && rs1.support[:total]).to eq 5
         expect(rs1.data.pluck(:word)).to eq(
           ["chocolate fingers", "chocolate fish", "chocolate fondant", "chocolate frosting", "chocolate fruit"]
         )
 
         t2 = "chocolate so"
-        rs2 = SearchService.call(relation: scoped_relation, search: Search.new(term: t2))
+        rs2 = search(scoped_relation, term: t2)
         expect(rs2.data.count && rs2.support[:total]).to eq 3
         expect(rs2.data.pluck(:word)).to eq(
           ["chocolate sorbet", "chocolate souffle", "chocolate soup"]
@@ -119,47 +120,38 @@ RSpec.describe SearchService, type: :service do
     context "macrons" do
       before(:each) do
         Refined::Search::Signs.with_macrons.each do |sign_attrs|
-          sign_attrs[:status] ||= "published"
-          sign_attrs[:conditions_accepted] = true
-
-          FactoryBot.create(:sign, sign_attrs)
+          FactoryBot.create(:sign, :published, sign_attrs)
         end
       end
 
       it "return result(s) regardless of macron" do
-        expect(
-          SearchService.call(
-            relation: scoped_relation, search: Search.new(term: "āporo")).data.first["maori"]
-        ).to eq("āporo")
+        expect(search(scoped_relation, term: "āporo").data.first["maori"])
+          .to eq("āporo")
+
+        expect(search(scoped_relation, term: "aporo").data.first["maori"])
+          .to eq("āporo")
+
+        expect(search(scoped_relation, term: "rahopūru").data.first["maori"]
+              ).to eq("rahopūru")
 
         expect(
-          SearchService.call(
-            relation: scoped_relation, search: Search.new(term: "aporo")).data.first["maori"]
-        ).to eq("āporo")
-
-        expect(
-          SearchService.call(
-            relation: scoped_relation, search: Search.new(term: "rahopūru")).data.first["maori"]
+          search(scoped_relation, term: "rahopuru").data.first["maori"]
         ).to eq("rahopūru")
 
         expect(
-          SearchService.call(relation: scoped_relation, search: Search.new(term: "rahopuru")).data.first["maori"]
-        ).to eq("rahopūru")
-
-        expect(
-          SearchService.call(relation: scoped_relation, search: Search.new(term: "pīti")).data.first["maori"]
+          search(scoped_relation, term: "pīti").data.first["maori"]
         ).to eq("pīti")
 
         expect(
-          SearchService.call(relation: scoped_relation, search: Search.new(term: "piti")).data.first["maori"]
+          search(scoped_relation, term: "piti").data.first["maori"]
         ).to eq("pīti")
 
         expect(
-          SearchService.call(relation: scoped_relation, search: Search.new(term: "parāoa")).data.first["maori"]
+          search(scoped_relation, term: "parāoa").data.first["maori"]
         ).to eq("kihu parāoa")
 
         expect(
-          SearchService.call(relation: scoped_relation, search: Search.new(term: "paraoa")).data.first["maori"]
+          search(scoped_relation, term: "paraoa").data.first["maori"]
         ).to eq("kihu parāoa")
       end
     end
@@ -173,34 +165,34 @@ RSpec.describe SearchService, type: :service do
         end
 
         it "returns result(s) total" do
-          rs1 = SearchService.call(relation: scoped_relation, search: Search.new(term: "a"))
+          rs1 = search(scoped_relation, term: "a")
           expect(rs1.data.count).to eq rs1.support[:total]
 
-          rs2 = SearchService.call(relation: scoped_relation, search: Search.new(term: "ap"))
+          rs2 = search(scoped_relation, term: "ap")
           expect(rs2.data.count).to eq rs2.support[:total]
 
-          rs3 = SearchService.call(relation: scoped_relation, search: Search.new(term: "app"))
+          rs3 = search(scoped_relation, term: "app")
           expect(rs3.data.count).to eq rs3.support[:total]
 
-          rs4 = SearchService.call(relation: scoped_relation, search: Search.new(term: "p"))
+          rs4 = search(scoped_relation, term: "p")
           expect(rs4.data.count).to eq rs4.support[:total]
 
-          rs5 = SearchService.call(relation: scoped_relation, search: Search.new(term: "pp"))
+          rs5 = search(scoped_relation, term: "pp")
           expect(rs5.data.count).to eq rs5.support[:total]
 
-          rs6 = SearchService.call(relation: scoped_relation, search: Search.new(term: "l"))
+          rs6 = search(scoped_relation, term: "l")
           expect(rs6.data.count).to eq rs6.support[:total]
 
-          rs7 = SearchService.call(relation: scoped_relation, search: Search.new(term: "le"))
+          rs7 = search(scoped_relation, term: "le")
           expect(rs7.data.count).to eq rs7.support[:total]
 
-          rs8 = SearchService.call(relation: scoped_relation, search: Search.new(term: "e"))
+          rs8 = search(scoped_relation, term: "e")
           expect(rs8.data.count).to eq rs8.support[:total]
 
-          rs9 = SearchService.call(relation: scoped_relation, search: Search.new(term: "123"))
+          rs9 = search(scoped_relation, term: "123")
           expect(rs9.data.count).to eq rs9.support[:total]
 
-          rs10 = SearchService.call(relation: scoped_relation, search: Search.new(term: "#$%"))
+          rs10 = search(scoped_relation, term: "#$%")
           expect(rs10.data.count).to eq rs10.support[:total]
         end
       end
@@ -208,35 +200,32 @@ RSpec.describe SearchService, type: :service do
       context "more than 10 results" do
         before(:each) do
           Refined::Search::Signs.with_chocolate.each do |sign_attrs|
-            sign_attrs[:status] ||= "published"
-            sign_attrs[:conditions_accepted] = true
-
-            FactoryBot.create(:sign, sign_attrs)
+            FactoryBot.create(:sign, :published, sign_attrs)
           end
         end
 
         it "returns page default limit and result(s) total" do
-          rs1 = SearchService.call(relation: scoped_relation, search: Search.new(term: "ch"))
+          rs1 = search(scoped_relation, term: "ch")
           expect(rs1.data.count).to eq Search::DEFAULT_LIMIT
           expect(rs1.support[:total]).to eq 20
 
-          rs2 = SearchService.call(relation: scoped_relation, search: Search.new(term: "oco"))
+          rs2 = search(scoped_relation, term: "oco")
           expect(rs2.data.count).to eq Search::DEFAULT_LIMIT
           expect(rs2.support[:total]).to eq 20
 
-          rs3 = SearchService.call(relation: scoped_relation, search: Search.new(term: "choc"))
+          rs3 = search(scoped_relation, term: "choc")
           expect(rs3.data.count).to eq Search::DEFAULT_LIMIT
           expect(rs3.support[:total]).to eq 20
 
-          rs4 = SearchService.call(relation: scoped_relation, search: Search.new(term: "a"))
+          rs4 = search(scoped_relation, term: "a")
           expect(rs4.data.count).to eq Search::DEFAULT_LIMIT
           expect(rs4.support[:total]).to eq 20
 
-          rs5 = SearchService.call(relation: scoped_relation, search: Search.new(term: "chocolate"))
+          rs5 = search(scoped_relation, term: "chocolate")
           expect(rs5.data.count).to eq Search::DEFAULT_LIMIT
           expect(rs5.support[:total]).to eq 20
 
-          rs6 = SearchService.call(relation: scoped_relation, search: Search.new(term: "ate"))
+          rs6 = search(scoped_relation, term: "ate")
           expect(rs6.data.count).to eq Search::DEFAULT_LIMIT
           expect(rs6.support[:total]).to eq 20
         end
@@ -252,8 +241,8 @@ RSpec.describe SearchService, type: :service do
 
       context "by recent" do
         example "be in the expected order" do
-          publish_dates = SearchService.call(relation: scoped_relation, search: Search.new(term: "a", sort: "recent"))
-                                       .data.map { |hsh| hsh["published_at"] }
+          publish_dates = search(scoped_relation, term: "a", sort: "recent")
+                          .data.map { |hsh| hsh["published_at"] }
 
           expect(publish_dates.sort.reverse).to eq publish_dates
         end
@@ -261,8 +250,8 @@ RSpec.describe SearchService, type: :service do
 
       context "by relevance" do
         example "be in the expected order" do
-          words = SearchService.call(relation: scoped_relation, search: Search.new(term: "ap", sort: "relevant"))
-                               .data.map { |hsh| hsh["word"] }
+          words = search(scoped_relation, term: "ap", sort: "relevant")
+                  .data.map { |hsh| hsh["word"] }
 
           expect(words.sort).to eq words
         end
@@ -270,8 +259,8 @@ RSpec.describe SearchService, type: :service do
 
       context "alphabetically ascending" do
         example "be in the expected order" do
-          words = SearchService.call(relation: scoped_relation, search: Search.new(term: "a", sort: "alpha_asc"))
-                               .data.map { |hsh| hsh["word"] }
+          words = search(scoped_relation, term: "a", sort: "alpha_asc")
+                  .data.map { |hsh| hsh["word"] }
 
           expect(words.sort).to eq words
         end
@@ -279,8 +268,8 @@ RSpec.describe SearchService, type: :service do
 
       context "alphabetically descending" do
         example "be in the expected order" do
-          words = SearchService.call(relation: scoped_relation, search: Search.new(term: "a", sort: "alpha_desc"))
-                               .data.map { |hsh| hsh["word"] }
+          words = search(scoped_relation, term: "a", sort: "alpha_desc")
+                  .data.map { |hsh| hsh["word"] }
 
           expect(words.sort.reverse).to eq words
         end
@@ -289,6 +278,10 @@ RSpec.describe SearchService, type: :service do
   end
 
   private
+
+  def search(relation, params)
+    SearchService.call(relation: relation, search: Search.new(params))
+  end
 
   def scoped_relation
     Pundit.policy_scope(user, Sign)
