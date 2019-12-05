9f09ed7df749572aad3e83f1633f37887df4aebc
Laura Suzuki <laura@rabidtech.co.nz>
2019-12-05 13:57:38 +1300

    Add display_avatar method

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -2,4 +2,12 @@ module AvatarHelper
   def transform(avatar)
     avatar.variant(resize_to_fill: [64, 64]).processed.service_url
   end
+
+  def display_avatar(user, classes="")
+    if user.avatar.attached?
+      content_tag(:img, nil, src: transform(user.avatar), class: classes)
+    else
+      inline_svg("media/images/avatar.svg", title: "Avatar", aria: true, class: classes)
+    end
+  end
 end
