ed3616cc45b0b66d5a16568cf9fa5fd8e5cd4cad
Trevor Hose <trevor.hose@ackama.com>
2019-12-16 11:29:23 +1300

    Add js snippet to use chosen

---
+++ app/frontend/components/chosen-topics.js
@@ -0,0 +1,8 @@
+$(() =>
+  $(".js-chosen-topics").chosen({
+    placeholder_text_multiple: "Select some topics",
+    single_backstroke_delete: false,
+    inherit_select_classes: true,
+    width: "100%"
+  })
+)
