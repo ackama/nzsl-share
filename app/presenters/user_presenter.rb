f6674009f9edd74bc76be3bf02b95628d035c482
Laura Suzuki <laura@rabidtech.co.nz>
2019-12-18 15:54:27 +1300

    Access avatar helper method from user presenter

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -3,17 +3,7 @@ class UserPresenter < ApplicationPresenter
   delegate_missing_to :user
   delegate :to_param, to: :user
 
-  def avatar
-    if user.avatar.attached?
-      transform(user.avatar)
-    else
-      "media/images/avatar.svg"
-    end
-  end
-
-  private
-
-  def transform(avatar)
-    avatar.variant(resize_to_fill: [64, 64])
+  def avatar(classes="")
+    h.display_avatar(user, classes)
   end
 end
