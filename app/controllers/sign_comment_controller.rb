3bcfcbd10ab7bf5287cf6aec58ee163896cb149b
Trevor Hose <trevor.hose@ackama.com>
2020-01-13 06:53:56 +1300

    Controller for sigh comments

---
+++ app/controllers/sign_comment_controller.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class SignCommentController < ApplicationController
+  def create
+    @sign = fetch_sign
+    authorize @sign, policy_class: SignCommentPolicy
+    @sign.sign_comments.new(build_comment).save
+    refresh_comments
+  end
+
+  def update
+    @sign = fetch_sign
+    authorize @sign, policy_class: SignCommentPolicy
+    @sign.sign_comments.find_by(id: id).update(comment: comment_param[:comment])
+    refresh_comments
+  end
+
+  def destroy
+    @sign = fetch_sign
+    authorize @sign, policy_class: SignCommentPolicy
+    @sign.sign_comments.find_by(id: id).destroy
+    refresh_comments
+  end
+
+  def appropriate
+    @sign = fetch_sign
+    authorize @sign, policy_class: SignCommentPolicy
+    @sign.sign_comments.find_by(id: id).update(appropriate: false)
+    refresh_comments
+  end
+
+  private
+
+  def refresh_comments
+    render partial: "sign_comments/refresh"
+  end
+
+  def comment_param
+    params.require(:sign_comment).permit(:comment)
+  end
+
+  def build_comment
+    { comment: comment_param[:comment], status: @sign.status, sign: @sign, user: current_user }
+  end
+
+  def fetch_sign
+    policy_scope(Sign).find_by!(id: sign_id)
+  end
+
+  def sign_id
+    params[:sign_id].to_i
+  end
+
+  def id
+    params[:id].to_i
+  end
+end
