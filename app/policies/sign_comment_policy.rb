e26fbbf488b386910ed9060cdaa2d17c49a182ec
Trevor Hose <trevor.hose@ackama.com>
2020-01-13 06:54:38 +1300

    Policy for sign comments
    * TODO: add more robust checks when we deal with collaboration

---
+++ app/policies/sign_comment_policy.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class SignCommentPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
+
+  def appropriate?
+    true
+  end
+end
