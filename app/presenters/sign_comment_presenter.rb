cf9cbab1809ccf6e515b97fd0a72302b2569f049
Trevor Hose <trevor.hose@ackama.com>
2020-01-14 08:36:35 +1300

    Add presenter for sign comments

---
+++ app/presenters/sign_comment_presenter.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class SignCommentPresenter < ApplicationPresenter
+  presents :sign_comment
+  delegate_missing_to :sign_comment
+
+  def friendly_date
+    created = sign_comment.created_at
+
+    return h.localize(created, format: "%-d %b %Y") unless created.today?
+
+    "Today at #{h.localize(sign_comment.created_at, format: "%I:%M %p")}"
+  end
+
+  def username
+    return "anonymous" if sign_comment.user.blank?
+
+    sign_comment.user.username
+  end
+
+  def html_comment
+    h.simple_format(sign_comment.comment)
+  end
+end
