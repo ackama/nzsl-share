2e173c0ea97d80cc9275c9d0a933d002f86d4c0d
Laura Suzuki <laura@rabidtech.co.nz>
2020-01-15 16:49:10 +1300

    Add validations to collaboration model

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,7 @@
 class Collaboration < ApplicationRecord
   belongs_to :folder
   belongs_to :collaborator, class_name: :User
+
+  validates :folder, presence: true
+  validates :collaborator, presence: true
 end
