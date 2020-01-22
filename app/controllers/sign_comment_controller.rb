8e3bb37bdc1140a8cadb5eb8181dfaa852b643c8
Trevor Hose <trevor.hose@ackama.com>
2020-01-23 05:47:31 +1300

    Refactor ctrl to policy check comment not sign

--- app/controllers/sign_comment_controller.rb
+++ app/controllers/sign_comment_controller.rb
@@ -3,51 +3,85 @@
 class SignCommentController < ApplicationController
   def create
     @sign = fetch_sign
-    authorize @sign, policy_class: SignCommentPolicy
-    @sign.sign_comments.new(build_comment).save
+    @sign_comment = SignComment.new(build_text_comment)
+    authorize @sign_comment
+    @sign_comment.save
+    @sign.reload
     refresh_comments
   end
 
   def update
     @sign = fetch_sign
-    authorize @sign, policy_class: SignCommentPolicy
-    @sign.sign_comments.find_by(id: id).update(comment_param)
+    @sign_comment = fetch_sign_comment
+    authorize @sign_comment
+    @sign_comment.update(comment: comment_param[:comment], display: true)
+    @sign.reload
     refresh_comments
   end
 
   def destroy
     @sign = fetch_sign
-    authorize @sign, policy_class: SignCommentPolicy
-    @sign.sign_comments.find_by(id: id).destroy
+    @sign_comment = fetch_sign_comment
+    authorize @sign_comment
+    @sign_comment.destroy
+    @sign.reload
     refresh_comments
   end
 
   def reply
     @sign = fetch_sign
-    authorize @sign, policy_class: SignCommentPolicy
-    @sign.sign_comments.new(build_comment.merge(parent_id: id)).save
+    @sign_comment = SignComment.new(build_text_comment.merge(parent_id: id))
+    authorize @sign_comment
+    @sign_comment.save
+    @sign.reload
     refresh_comments
   end
 
   def appropriate
     @sign = fetch_sign
-    authorize @sign, policy_class: SignCommentPolicy
-    @sign.sign_comments.find_by(id: id).update(appropriate: false)
+    @sign_comment = fetch_sign_comment
+    authorize @sign_comment
+    @sign_comment.update(appropriate: false)
+    @sign.reload
+    refresh_comments
+  end
+
+  def video
+    @sign = fetch_sign
+    @sign_comment = SignComment.new(build_partial_video_comment)
+    authorize @sign_comment
+    @sign_comment.save
+    @sign.reload
     refresh_comments
   end
 
   private
 
   def refresh_comments
-    render partial: "sign_comments/refresh"
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: @sign) }
+      format.js   { render partial: "sign_comments/refresh" }
+    end
   end
 
   def comment_param
     params.require(:sign_comment).permit(:comment)
   end
 
-  def build_comment
-    { comment: comment_param[:comment], status: @sign.status, sign: @sign, user: current_user }
+  def video_param
+    params.require(:sign_comment).permit(:video)
+  end
+
+  def build_text_comment
+    { comment: comment_param[:comment], sign_status: @sign.status, sign: @sign, user: current_user }
+  end
+
+  def build_partial_video_comment
+    build_text_comment.merge(video: video_param[:video], display: false)
+  end
+
+  def fetch_sign_comment
+    policy_scope(SignComment).find_by!(id: id)
   end
 
   def fetch_sign
