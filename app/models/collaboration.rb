f9c58bc805c6e25c117c0940e90643f6644e6425
Laura Suzuki <laura@rabidtech.co.nz>
2020-01-23 11:00:51 +1300

    Validate folder_id and collaborator_id rather than folder and collaborator

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,7 +1,9 @@
 class Collaboration < ApplicationRecord
+  attr_accessor :identifier
   belongs_to :folder
   belongs_to :collaborator, class_name: :User
 
-  validates :folder, presence: true
-  validates :collaborator, presence: true
+  validates :folder_id, presence: true
+  validates :collaborator_id, presence: true
+  # validates :identifier, presence: true
 end
