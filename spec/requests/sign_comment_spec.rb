5b1c2e8e1d0509ec20ebda128b87e096bdf71d77
Trevor Hose <trevor.hose@ackama.com>
2020-01-27 08:55:28 +1300

    Spec for appropriate update

--- spec/requests/sign_comment_spec.rb
+++ spec/requests/sign_comment_spec.rb
@@ -16,6 +16,10 @@ RSpec.describe "sign_comment", type: :request do
     ->(sign, sign_comment) { delete "/signs/#{sign.id}/comment/#{sign_comment.id}" }
   end
 
+  let(:appropriate) do
+    ->(sign, sign_comment) { patch "/signs/#{sign.id}/comment/#{sign_comment.id}/appropriate" }
+  end
+
   let(:reply) do
     lambda { |sign, sign_comment|
       post "/signs/#{sign.id}/comment/#{sign_comment.id}/reply", params: { sign_comment: reply_params }
@@ -52,6 +56,19 @@ RSpec.describe "sign_comment", type: :request do
         expect(sign.sign_comments.count).to eq 1
         destroy.call(sign, sign.sign_comments.first)
         expect(sign.sign_comments.count).to eq 0
+        expect(response).to redirect_to sign_path(sign)
+      end
+    end
+
+    context "appropriate" do
+      it "it flags a comment as inapprppriate" do
+        user.update(approved: true)
+        expect(sign.sign_comments.count).to eq 0
+        create.call(sign)
+        expect(sign.sign_comments.count).to eq 1
+        expect(sign.sign_comments.first.appropriate).to be true
+        appropriate.call(sign, sign.sign_comments.first)
+        expect(sign.sign_comments.first.appropriate).to be false
         expect(response).to redirect_to sign_path(sign)
       end
     end
