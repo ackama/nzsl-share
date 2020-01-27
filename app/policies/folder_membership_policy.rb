a5bfe80fcd2ecd456dbe0e88fc809c5735fa6371
Laura Suzuki <laura@rabidtech.co.nz>
2020-01-28 12:21:15 +1300

    Only collaborators can create/destroy folder memberships

--- app/policies/folder_membership_policy.rb
+++ app/policies/folder_membership_policy.rb
@@ -1,26 +1,13 @@
 class FolderMembershipPolicy < ApplicationPolicy
   def create?
-    owns_folder? || collaborator?
+    collaborator?
   end
 
   def destroy?
-    owns_folder? || collaborator?
-  end
-
-  class Scope < Scope
-    def resolve
-      folder_ids = Folder.where(user: user).pluck(:id)
-      collab_folder_ids = Folder.joins(:collaborations).where(collaborations: { collaborator_id: user.id }).pluck(:id)
-      all_folder_ids = folder_ids + collab_folder_ids
-      scope.where(id: all_folder_ids).distinct
-    end
+    collaborator?
   end
 
   private
-
-  def owns_folder?
-    record.folder.user_id == user.id
-  end
 
   def collaborator?
     record.folder.collaborators.map(&:id).include? user.id
