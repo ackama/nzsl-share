14c46a38d6e74cf0444896322584aab4c66b0b70
Laura Suzuki <laura@rabidtech.co.nz>
2020-01-28 10:47:41 +1300

    Move scope out of private methods

--- app/policies/folder_membership_policy.rb
+++ app/policies/folder_membership_policy.rb
@@ -7,6 +7,15 @@ class FolderMembershipPolicy < ApplicationPolicy
     owns_folder? || collaborator?
   end
 
+  class Scope < Scope
+    def resolve
+      folder_ids = Folder.where(user: user).pluck(:id)
+      collab_folder_ids = Folder.joins(:collaborations).where(collaborations: { collaborator_id: user.id }).pluck(:id)
+      all_folder_ids = folder_ids + collab_folder_ids
+      scope.where(id: all_folder_ids).distinct
+    end
+  end
+
   private
 
   def owns_folder?
@@ -15,14 +24,5 @@ class FolderMembershipPolicy < ApplicationPolicy
 
   def collaborator?
     record.folder.collaborators.map(&:id).include? user.id
-  end
-
-  class Scope < Scope
-    def resolve
-      folder_ids = Folder.where(user: user).pluck(:id)
-      collab_folder_ids = Folder.joins(:collaborations).where(collaborations: { collaborator_id: user.id }).pluck(:id)
-      all_folder_ids = folder_ids + collab_folder_ids
-      scope.where(id: all_folder_ids).distinct
-    end
   end
 end
