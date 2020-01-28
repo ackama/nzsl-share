8774227b1d83244cbfa922fa298a6d9df2c60723
Laura Suzuki <laura@rabidtech.co.nz>
2020-01-28 23:47:43 +1300

    Add subject to collaboration mailer #success, accept the collaborator argument

--- app/mailers/collaboration_mailer.rb
+++ app/mailers/collaboration_mailer.rb
@@ -1,7 +1,6 @@
 class CollaborationMailer < ApplicationMailer
-  def success(collaboration)
-    @folder = Folder.find(collaboration.folder_id)
-    @collaborator = User.find(collaboration.collaborator_id)
-    mail to: @collaborator.email
+  def success(collaborator)
+    @collaborator = collaborator
+    mail(to: @collaborator.email, subject: "You have been added as a collaborator")
   end
 end
