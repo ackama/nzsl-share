53df9e16431965b159ed2c7e135cd960bf8aa5a1
Laura Suzuki <laura@rabidtech.co.nz>
2020-01-29 12:59:31 +1300

    User I18n for subject line

--- app/mailers/collaboration_mailer.rb
+++ app/mailers/collaboration_mailer.rb
@@ -1,6 +1,6 @@
 class CollaborationMailer < ApplicationMailer
   def success(collaborator)
     @collaborator = collaborator
-    mail(to: @collaborator.email, subject: "You have been added as a collaborator")
+    mail(to: @collaborator.email)
   end
 end
