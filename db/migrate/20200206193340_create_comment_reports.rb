098c6bc5873454c9a99b9176cbe8e6b714dcc587
Josh McArthur <joshua.mcarthur@gmail.com>
2020-02-10 08:51:40 +1300

    Remove 'resolved_by' from comment reports

--- db/migrate/20200206193340_create_comment_reports.rb
+++ db/migrate/20200206193340_create_comment_reports.rb
@@ -3,7 +3,6 @@ class CreateCommentReports < ActiveRecord::Migration[6.0]
     create_table :comment_reports do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :comment, null: false, foreign_key: { to_table: :sign_comments }
-      t.belongs_to :resolved_by, null: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
