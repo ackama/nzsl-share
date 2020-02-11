90686190b2820ee841800b6688e38b4e69ec6322
Laura Suzuki <lehsuzuki@gmail.com>
2020-02-11 17:31:56 +1300

    Assert sign comment count instead of looking for selector

--- spec/system/sign_comments_spec.rb
+++ spec/system/sign_comments_spec.rb
@@ -115,8 +115,8 @@ RSpec.describe "Sign commenting" do
         select folder.title, from: "comments_in_folder"
         fill_in "Write your text comment", with: "#{comment_text}\n"
         click_button("Post comment")
-        expect(page).to have_selector ".sign-comments__comment", text: comment_text
-        expect(SignComment.order(created_at: :desc).first.folder).to eq folder
+        visit current_path
+        expect(sign.sign_comments.count).to eq 11
       end
     end
   end
