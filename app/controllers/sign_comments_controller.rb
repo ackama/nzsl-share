222ea4182345dae2516680045ff9c604a9530ec2
Laura Suzuki <laura@rabidtech.co.nz>
2020-02-13 12:02:04 +1300

    REinstate location arg

--- app/controllers/sign_comments_controller.rb
+++ app/controllers/sign_comments_controller.rb
@@ -45,7 +45,7 @@ class SignCommentsController < ApplicationController
 
   private
 
-  def refresh_comments
+  def refresh_comments(location: nil)
     respond_to do |format|
       format.html { location ? redirect_to(location) : redirect_back(fallback_location: @sign) }
       format.js do
