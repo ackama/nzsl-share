029a616e76166832ed2b342edfba7c415eb7e57c
Trevor Hose <trevor.hose@ackama.com>
2020-02-19 13:47:20 +1300

    Use a view helper for nzsl signature
    * remove the text signature from app mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,13 +5,4 @@ class ApplicationMailer < ActionMailer::Base
   def admins
     User.where(administrator: true).map(&:email)
   end
-
-  def nzsl_text_signature
-    "Kind regards\n\n" \
-    "NZSL Share Team\n\n" \
-    "NZSL Share: https://nzslshare.nz\n" \
-    "NZSL Online Dictionary: https://nzsl.nz\n" \
-    "Learn NZSL: http://www.learnnzsl.nz\n" \
-    "Email: DSRU@vuw.ac.nz"
-  end
 end
