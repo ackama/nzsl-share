642e6af520e95937f0c73cdab618e516e49f04d6
dan morrison <dan@rabidtech.co.nz>
2020-03-23 16:14:01 +1300

    add missing directories and files to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../../frontend/packs
 //= link_directory ../../frontend/foundation
 //= link_directory ../../frontend/components
+//= link_directory ../stylesheets
+//= link administrate/application.css
+//= link administrate/application.js
