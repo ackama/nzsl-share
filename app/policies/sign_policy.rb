d58356f40658b882c1b3d9c6b83ce5a408118e09
Trevor Hose <trevor.hose@ackama.com>
2020-03-26 19:42:34 +1300

    Return resolve_moderator result set early

--- app/policies/sign_policy.rb
+++ app/policies/sign_policy.rb
@@ -119,6 +119,8 @@ class SignPolicy < ApplicationPolicy
               .joins(folder: :folder_memberships)
               .pluck("folder_memberships.sign_id")
 
+        return resolve_moderator if collaboration_sign_ids.blank?
+
         Sign.where(id: collaboration_sign_ids).or(resolve_moderator)
       elsif user
         resolve_user
