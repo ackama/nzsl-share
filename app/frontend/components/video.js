7d8da90f5b16b2093ba1feaa263124c7d29f4e6e
Jacob Briggs <jacob.briggs@ackama.com>
2021-03-16 15:05:44 +1300

    commiting to ensure I don't loose code really. I have a more jqueryish solution here, where the "onclick" event propagates up to document so the event handler works but the "onended" event doesn't so that event handler doesn't.

--- app/frontend/components/video.js
+++ app/frontend/components/video.js
@@ -1,12 +1,34 @@
-// eslint-disable-next-line no-undef
-togglePlayPause = function(el) {
-  if (el.classList.contains("video--hero")) {
-    $("img.video__poster").toggle();
-    el.classList.toggle("video--hero--large");
-  }
-
-  el.classList.toggle("video--playing");
-  el.paused ? el.play() : el.pause();
+/* the ended event apparently doesn't bubble up to document so this never fires */
+$(document).on("ended", ".video--playing", function() {
+  console.log("ON ENDED");
+  $(this).get(0).classList.remove("video--playing");
 
   return false;
-};
+});
+
+/* This one fires though. Perhaps its not the best way to do this though */
+document.addEventListener(
+  "ended",
+  function(e) {
+    if ($(e.target).is("video")) {
+      console.log("ON ENDED 2");
+      e.target.classList.remove("video--playing");
+    }
+  },
+  true
+);
+
+$(document).on("click", ".video__overlay", function() {
+  console.log("on click");
+  var videoElement = $(this).siblings(".video").get(0);
+
+  if (videoElement.classList.contains("video--hero")) {
+    $("img.video__poster").toggle();
+    videoElement.classList.toggle("video--hero--large");
+  }
+
+  videoElement.classList.toggle("video--playing");
+  videoElement.paused ? videoElement.play() : videoElement.pause();
+
+  return false;
+});
