49d364796c9f5069ccfb31e94d617e15359ebc99
Josh McArthur <joshua.mcarthur@gmail.com>
2021-11-28 21:18:01 +1300

    Wait for ajax with timeout rather than a fixed wait

--- spec/support/wait_for_ajax.rb
+++ spec/support/wait_for_ajax.rb
@@ -1,10 +1,9 @@
 # https://thoughtbot.com/blog/automatically-wait-for-ajax-with-capybara
 module WaitForAjax
   def wait_for_ajax
-    sleep 3
-    # Timeout.timeout(Capybara.default_max_wait_time) do
-    #   loop until finished_all_ajax_requests?
-    # end
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      loop until finished_all_ajax_requests?
+    end
   end
 
   def finished_all_ajax_requests?
