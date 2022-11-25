5f6cfd9664c40c7d7d38711a4f4c915c3db36bbd
Josh McArthur <joshua.mcarthur@gmail.com>
2022-11-25 14:19:15 +1300

    Wrap use of autoloaded models in to_prepare

--- config/initializers/dictionary_sign.rb
+++ config/initializers/dictionary_sign.rb
@@ -1,6 +1,9 @@
 # Conditionally switch between dictionary sign models based on an environment variable.
 # This allows us to quickly switch between searching Freelex and Signbank data for signs.
 
-dictionary_mode = ENV["DICTIONARY_MODE"]&.downcase
-dictionary_sign_model = dictionary_mode == "freelex" ? FreelexSign : DictionarySign
-Rails.application.config.dictionary_sign_model = dictionary_sign_model
+Rails.application.reloader.to_prepare do
+  dictionary_mode = ENV["DICTIONARY_MODE"]&.downcase
+  dictionary_sign_model = dictionary_mode == "freelex" ? FreelexSign : DictionarySign
+  Rails.application.config.dictionary_sign_model = dictionary_sign_model
+
+end
