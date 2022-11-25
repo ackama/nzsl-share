20fee23308631e2f58ea73143c577809795af885
Josh McArthur <joshua.mcarthur@gmail.com>
2022-11-25 14:20:57 +1300

    Add a generated column to the dictionary SQLite file so that we can reference the 'word' column

--- config/initializers/dictionary_sign.rb
+++ config/initializers/dictionary_sign.rb
@@ -6,4 +6,13 @@ Rails.application.reloader.to_prepare do
   dictionary_sign_model = dictionary_mode == "freelex" ? FreelexSign : DictionarySign
   Rails.application.config.dictionary_sign_model = dictionary_sign_model
 
+  ##
+  # All other tables make heavy use of a 'word' column. Add an alias for it here so that
+  # we can use common queries and ordering.
+  # There's no ADD COLUMN IF NOT EXISTS, so we just handle the error
+  begin
+    DictionarySign.connection.execute("ALTER TABLE words ADD COLUMN word text GENERATED ALWAYS AS (gloss)")
+  rescue ActiveRecord::StatementInvalid => e
+    raise e unless e.message == "SQLite3::SQLException: duplicate column name: word"
+  end
 end
