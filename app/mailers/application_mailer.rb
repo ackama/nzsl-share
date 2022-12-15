c1894b9d72886d28aa67cd528c1629e000960dfe
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-15 15:15:01 +1300

    Include mailer helpers in all mailer classes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV["MAIL_FROM"]
   layout "mailer"
+  helper :mailers
 
   def admins
     User.where(administrator: true).map(&:email)
+  end
+
+  def moderators
+    User.where(moderator: true).map(&:email)
   end
 end
