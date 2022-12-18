8635a3b9629a0d3d1b59c42ddb05b099c4f6fe8a
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-19 12:09:53 +1300

    Don't allow users to be destroyed when they have signs

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   attr_writer :login
 
   has_many :folders, dependent: :destroy
-  has_many :signs, foreign_key: :contributor_id, inverse_of: :contributor, dependent: :nullify
+  has_many :signs, foreign_key: :contributor_id, inverse_of: :contributor, dependent: :restrict_with_error
   has_many :collaborations, foreign_key: :collaborator_id, inverse_of: :collaborator, dependent: :destroy
 
   has_many :sign_comments, dependent: :nullify
