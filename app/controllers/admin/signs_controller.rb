c22cb513fd82c7abb60011adfcab80f067f879fc
Josh McArthur <josh.mcarthur@ackama.com>
2022-12-22 12:10:28 +1300

    NZSL-63: Filter sign moderation to pending signs by default (#484)
    * NZSL-63: Filter sign moderation to pending signs by default
    
    * Visit admin sign moderation with the correct filter for expected records to display
    
    * Increase wait time for checking that video processing completes
    
    I suspect that Github actions is doing some performance stuff to slow down ffmpeg, which is exceeding our wait time. To try and work around this, allow this specific test to wait longer for the element to appear before failing

--- app/controllers/admin/signs_controller.rb
+++ app/controllers/admin/signs_controller.rb
@@ -2,6 +2,11 @@
 
 module Admin
   class SignsController < Admin::ApplicationController
+    def index
+      params[:search] ||= "pending:"
+      super
+    end
+
     def scoped_resource
       policy_scope(Sign).where.not(status: :personal)
     end
