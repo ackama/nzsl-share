6c9988c701ad2a80cc80bdb179e638051cac765a
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-23 09:38:09 +1300

    Show comment count on sign card

--- spec/views/signs/_card.html.erb_spec.rb
+++ spec/views/signs/_card.html.erb_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "signs/_card.html.erb", type: :view do
   let(:user) { FactoryBot.build(:user) }
   let(:sign) { FactoryBot.build_stubbed(:sign, :published, contributor: user) }
-  let(:presenter) { SignPresenter.new(sign, ApplicationController.new) }
+  let(:presenter) { SignPresenter.new(sign, view) }
   subject(:rendered) { render "signs/card", sign: sign }
 
   before { stub_authorization }
@@ -25,7 +25,6 @@ RSpec.describe "signs/_card.html.erb", type: :view do
   end
 
   it "shows the sign status if the user can access the record" do
-    sign_in user
     expect(rendered).to have_content "Public"
     title = Capybara.string(rendered).find("#sign_status")["title"]
     expect(title).to eq I18n.t!("signs.published.description")
@@ -56,6 +55,40 @@ RSpec.describe "signs/_card.html.erb", type: :view do
     expect(rendered).to have_link "Disagree"
   end
 
-  it "shows the folder button"
-  it "shows the folder button in an active state if the sign is already in a user folder"
+  it "shows the folder button when signed in" do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:user_signed_in?).and_return(true)
+    expect(rendered).to have_button(class: "sign-card__folders__button")
+    expect(rendered).to have_selector(".sign-card__folders__button > svg > title", text: "Folders")
+  end
+
+  it "shows a fake folder button to the sign in page when signed out" do
+    sign_out :user
+    expect(rendered).to have_link("Folders", href: new_user_session_path)
+  end
+
+  it "shows the folder button in an active state if the sign is already in a user folder" do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:user_signed_in?).and_return(true)
+    allow(view).to receive(:present).with(sign).and_return(presenter)
+    expect(presenter).to receive(:available_folders).at_least(1).times.and_return([Folder.new])
+
+    expect(rendered).to have_button(
+      class: %w[sign-card__folders__button sign-card__folders__button--in-folder])
+    expect(rendered).to have_selector(
+      ".sign-card__folders__button > svg > title",
+      text: "Folders")
+  end
+
+  it "shows a count of comments for the sign that the user can access" do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:user_signed_in?).and_return(true)
+    allow(view).to receive(:present).with(sign).and_return(presenter)
+    expect(presenter).to receive(:comments_count).and_return(10)
+    expect(rendered).to have_selector(".sign-card__controls--comments", text: "Comments\n\n  10")
+  end
+
+  it "does not show the comment count when the user is not signed in" do
+    expect(rendered).not_to have_selector(".sign-card__controls--comments")
+  end
 end
