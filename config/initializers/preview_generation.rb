7b5b314b8c24c55b6a66641158ffb964c838b51b
Cameron Hopkinson <cameron.hopkinson@ackama.com>
2023-01-20 13:35:50 +1300

    Override thumbnail generation to generate thumbnails a quarter of the way through a video

---
+++ config/initializers/preview_generation.rb
@@ -0,0 +1,11 @@
+ActiveStorage::Previewer::VideoPreviewer.class_eval do
+  def draw_relevant_frame_from(file, &block)
+    draw self.class.ffmpeg_path, "-i", file.path, *video_preview_arguments_with_mid_video_start_time, "-", &block
+  end
+
+  def video_preview_arguments_with_mid_video_start_time
+    # Ensures that the blob has been analysed, otherwise there will be no duration and it will default to the start of the video
+    blob.analyze
+    Shellwords.split(ActiveStorage.video_preview_arguments + " -ss #{blob.metadata.fetch(:duration, 0) / 4}")
+  end
+end
