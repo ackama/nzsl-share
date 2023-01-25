9eba6ae4e345c5a00920cb2071ab54ec69defdd6
Josh McArthur <joshua.mcarthur@gmail.com>
2023-01-26 08:49:00 +1300

    Use an administration pack for Administrate pages to avoid loading unnecessary JS, and some JS that doubles-up administrate functionality

---
+++ app/frontend/packs/administration.js
@@ -0,0 +1,17 @@
+/* eslint no-console:0 */
+/* global require */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'administration' %> to the appropriate
+// layout file, like app/views/layouts/admin/application.html.erb
+
+
+require.context("../images", true);
+require("@rails/activestorage").start();
+
+import "foundation";
+import "../application.scss";
+import "../components/header";
