f0dc3700ef896749f733ca0714d68d9041940c59
Josh McArthur <joshua.mcarthur@gmail.com>
2023-01-25 15:03:05 +1300

    Update view specs to stub current user when checking contribution limits:

--- spec/views/signs/new.html.erb_spec.rb
+++ spec/views/signs/new.html.erb_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "signs/new.html.erb", type: :view do
 
   before do
     assign(:sign, sign)
+    allow(view).to receive(:current_user).and_return(User.new)
     render
   end
 
@@ -13,6 +14,13 @@ RSpec.describe "signs/new.html.erb", type: :view do
   end
 
   it "shows the multiple file upload area", upload_mode: :uppy do
-    expect(rendered).to have_selector("[data-controller='file-upload']")
+    expect(rendered).to have_selector("[data-controller='create-sign']")
+  end
+
+  it "passes the contribution limit to the create sign process", upload_mode: :uppy do
+    view.current_user.contribution_limit = 10
+    allow(view.current_user.signs).to receive(:count).and_return(5)
+    rendered = render
+    expect(rendered).to have_selector("[data-create-sign-contribution-limit-value='5']")
   end
 end
