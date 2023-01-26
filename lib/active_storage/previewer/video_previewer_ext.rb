33f2a489078c880d4860c1b82a41735647442e14
Josh McArthur <joshua.mcarthur@gmail.com>
2023-01-27 11:34:19 +1300

    Rename ActiveStorage::Previewer::VideoPreviewer extension to not conflict with main file in load path

--- lib/active_storage/previewer/video_previewer_ext.rb
+++ lib/active_storage/previewer/video_previewer_ext.rb
@@ -4,14 +4,17 @@ require "active_storage/previewer/video_previewer"
 # Reopens class to add support for extracting a frame at a specific location, rather than
 # from the first frame.
 ActiveStorage::Previewer::VideoPreviewer.class_eval do
+  private
+
   def draw_relevant_frame_from(file, &block)
-    draw self.class.ffmpeg_path, "-i", file.path, *video_preview_arguments_with_mid_video_start_time, "-", &block
+    draw self.class.ffmpeg_path, "-i", file.path, *video_preview_arguments_with_frame_time, "-", &block
   end
 
-  def video_preview_arguments_with_mid_video_start_time
+  def video_preview_arguments_with_frame_time(proportion: 0.25)
     # Ensures that the blob has been analysed, otherwise there will be no duration and
     # it will default to the start of the video
     blob.analyze
-    Shellwords.split(ActiveStorage.video_preview_arguments + " -ss #{blob.metadata.fetch(:duration, 0) / 4}")
+
+    Shellwords.split(ActiveStorage.video_preview_arguments + " -ss #{blob.metadata.fetch(:duration, 0) * proportion}")
   end
 end
