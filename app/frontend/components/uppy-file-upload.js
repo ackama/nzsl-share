b8cceac233e0152792e89b69c2fc240317502fe5
Josh McArthur <joshua.mcarthur@gmail.com>
2023-01-30 11:23:29 +1300

    Adjust locales for webcam upload

--- app/frontend/components/uppy-file-upload.js
+++ app/frontend/components/uppy-file-upload.js
@@ -11,7 +11,18 @@ const uppyFileUpload = (container, options = {}) => {
   });
 
   uppy.use(DropTarget, { target: document.body });
-  uppy.use(Webcam,  { modes: "video-only", countdown: true });
+  uppy.use(Webcam,  {
+    modes: "video-only",
+    countdown: true,
+    locale: {
+      strings: {
+        pluginNameCamera: "Video recording",
+        noCameraDescription: "In order to record video with your camera, please connect a camera device.",
+        allowAccessDescription: "In order to record video with your camera, please allow camera access for this site."
+      }
+    }
+  });
+
   uppy.use(ActiveStorageUpload, { directUploadUrl: "/rails/active_storage/direct_uploads" });
 
   uppy.use(Dashboard, {
