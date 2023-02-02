3f33d25a42fc95fb9f29e6155b70b219d8f1ac4d
Cameron Hopkinson <hopkincame@MSI>
2023-02-02 16:44:23 +1300

    Added ability to sort signs list

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -2,6 +2,11 @@ class SignsController < ApplicationController # rubocop:disable Metrics/ClassLen
   before_action :authenticate_user!, except: %i[show]
   after_action :mark_comments_as_read, only: :show
 
+  SORT_OPTIONS = {
+    alphabetical: "word ASC",
+    most_recent: "created_at DESC"
+  }.freeze
+
   def index
     @signs = signs.where(contributor: current_user).page(params[:page])
     authorize @signs
@@ -79,7 +84,8 @@ class SignsController < ApplicationController # rubocop:disable Metrics/ClassLen
   end
 
   def signs
-    policy_scope(Sign).for_cards.order(word: :asc)
+    sort_by = SORT_OPTIONS[params[:sort_by].try(:to_sym)] || SORT_OPTIONS[:alphabetical]
+    policy_scope(Sign).for_cards.order(sort_by)
   end
 
   def build_sign(builder: SignBuilder.new)
