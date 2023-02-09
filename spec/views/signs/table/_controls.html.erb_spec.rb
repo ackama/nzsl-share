708819cd4413aa09a4af2ca9755382ab455fd84f
Cameron Hopkinson <hopkincame@MSI>
2023-02-09 15:41:18 +1300

    Made current user required for sign controls

--- spec/views/signs/table/_controls.html.erb_spec.rb
+++ spec/views/signs/table/_controls.html.erb_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "signs/table/_controls.html.erb", type: :view do
   before { stub_policy_scope(Topic) }
 
   it "allows assignment of a topic" do
+    allow(view).to receive(:current_user).and_return(FactoryBot.create(:user))
     rendered = render
     expect(rendered).to have_select("Assign topics", with_options: Topic.pluck(:name))
     expect(rendered).to have_button("Assign")
@@ -17,11 +18,6 @@ RSpec.describe "signs/table/_controls.html.erb", type: :view do
 
   it "doesn't show the submit for publishing button if the current user is not approved" do
     allow(view).to receive(:current_user).and_return(FactoryBot.create(:user))
-    rendered = render
-    expect(rendered).not_to have_button("Submit for publishing")
-  end
-
-  it "doesn't show the submit for publishing button if no user is logged in" do
     rendered = render
     expect(rendered).not_to have_button("Submit for publishing")
   end
