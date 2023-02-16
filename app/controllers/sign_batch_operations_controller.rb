bfaaaf8e406de809079a46bd43248a63fde34c56
Josh McArthur <josh.mcarthur@ackama.com>
2023-02-16 14:11:23 +1300

    NZSL-131: Improve bulk sign operation messaging (#547)
    * Implement handling for when no signs are selected
    
    * Add an echo batch command for use in testing when we don't want to cause side-effects to records
    
    * Don't raise when sign IDs are missing
    
    * Refactor batch operation messaging to handle missing sign IDs and mixed success
    
    * Update messaging in system specs
    
    * Fixes for mixed success messaging

--- app/controllers/sign_batch_operations_controller.rb
+++ app/controllers/sign_batch_operations_controller.rb
@@ -5,18 +5,17 @@ class SignBatchOperationsController < ApplicationController
 
   def create
     operation = resolve_operation
-    return head(:unprocessable_entity) unless operation
-
     authorize_operation = method(:authorize_record_operation)
     batch = BatchOperation.new(signs, operation, precondition: authorize_operation)
+    return if performed? # Stop processing if any of the prepartion methods have halted the request
+
     succeeded, failed = batch.process
 
     respond_to do |format|
       format.json { render json: { succeeded: succeeded, failed: failed } }
       format.html do
         redirect_to user_signs_path(sign_ids: sign_ids),
-                    notice: t(".success", succeeded_count: succeeded.size,
-                                          failed_count: failed.size)
+                    **flash_messages_for_result(succeeded, failed)
       end
     end
   end
@@ -29,6 +28,10 @@ class SignBatchOperationsController < ApplicationController
       method(:assign_topic)
     when :submit_for_publishing
       method(:submit_for_publishing)
+    when :echo
+      method(:echo)
+    else
+      head(:unprocessable_entity)
     end
   end
 
@@ -39,6 +42,12 @@ class SignBatchOperationsController < ApplicationController
 
   def assign_topic(record)
     record.topics << policy_scope(Topic).find(params.require(:topic_id))
+  end
+
+  ##
+  # Just used for testing, just succeed the record
+  def echo(record)
+    record
   end
 
   def submit_for_publishing(record)
@@ -53,10 +62,20 @@ class SignBatchOperationsController < ApplicationController
   end
 
   def sign_ids
-    params.require(:sign_ids)
+    params.fetch(:sign_ids, [])
   end
 
   def signs
     policy_scope(Sign.where(id: sign_ids)).limit(BULK_OPERATIONS_LIMIT)
+  end
+
+  def flash_messages_for_result(succeeded_records, failed_records)
+    if !succeeded_records.empty? && failed_records.empty?
+      { notice: t(".success", succeeded_count: succeeded_records.size) }
+    elsif !succeeded_records.empty? || !failed_records.empty?
+      { alert: t(".mixed_success", succeeded_count: succeeded_records.size, failed_count: failed_records.size) }
+    else
+      { alert: t(".failure_missing_sign_ids") }
+    end
   end
 end
