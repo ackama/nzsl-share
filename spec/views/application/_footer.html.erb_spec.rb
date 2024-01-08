e98ae81b5423c202e30dda1516ea416db79b7288
Josh McArthur <joshua.mcarthur@gmail.com>
2024-01-09 11:25:58 +1300

    Add a spec for a missing database version

--- spec/views/application/_footer.html.erb_spec.rb
+++ spec/views/application/_footer.html.erb_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe "application/_footer.html.erb", type: :view do
     render
     expect(rendered).to have_content "Dictionary database version: 20230101"
   end
+
+  it "does not display the DictionarySign version text in the footer when the version is missing" do
+    allow(DictionarySign).to receive(:version).and_return(nil)
+    render
+    expect(rendered).not_to have_content "Dictionary database version"
+  end
 end
