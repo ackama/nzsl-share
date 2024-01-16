5defd4c08c4f39ff218d57e43dffe24a0d634ad8
Josh McArthur <josh.mcarthur@ackama.com>
2024-01-17 12:23:06 +1300

    Revert "NZSL-166: Presign requests for dictionary assets when a keypair is configured (#603)" (#611)
    This reverts commit 32f87d1a2b24ff49fa9173aad0fa094820925f32. We have
    determined that private assets should never be presented from prerelease
    databases, and while the ability to export such videos from NZSL
    Signbank has already been removed, we want to ensure that it's not
    possible to present these videos by removing the ability to presign
    asset URLs within NZSL Share.

--- spec/models/dictionary_sign_spec.rb
+++ spec/models/dictionary_sign_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe DictionarySign, type: :model do
     expect(described_class.connection.adapter_name).to eq "SQLite"
   end
 
-  it "wraps the video attribute with a value object" do
-    sign = FactoryBot.create(:dictionary_sign, video: "https://example.com/video.mp4", id: SecureRandom.uuid)
-    expect(sign.video).to eq URI.parse("https://example.com/video.mp4")
-  end
-
-  it "guards against a blank video attribute value" do
-    sign = FactoryBot.create(:dictionary_sign, video: "", id: SecureRandom.uuid)
-    expect(sign.video).to be_nil
-  end
-
   it "filters out obscene entries by default" do
     sign = FactoryBot.create(:dictionary_sign, usage: "obscene", id: SecureRandom.uuid)
     expect(DictionarySign.unscoped.where(id: sign.id)).to eq [sign]
