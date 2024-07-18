cbffa17ef3b0933eae626d132c50def86a5cf46d
Gareth Jones <jones258@gmail.com>
2024-07-18 15:19:19 +1200

    build: serve web files

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,30 @@
 'use strict';
 
-const { generateWebpackConfig } = require('shakapacker');
+const { dirname } = require('path');
+const {
+  config: { source_path: sourcePath },
+  generateWebpackConfig
+} = require('shakapacker');
 
-module.exports = generateWebpackConfig({});
+module.exports = generateWebpackConfig({
+  module: {
+    rules: [
+      {
+        test: /\.(mp4|webm)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: pathData => {
+            const folders = dirname(pathData.filename)
+              .replace(`${sourcePath}`, '')
+              .split('/')
+              .filter(Boolean);
+
+            const foldersWithStatic = ['static', ...folders].join('/');
+
+            return `${foldersWithStatic}/[name]-[hash][ext][query]`;
+          }
+        }
+      }
+    ]
+  }
+});
