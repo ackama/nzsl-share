e7fc046471e18845c2410c1bef5b9cb07ac47795
Gareth Jones <jones258@gmail.com>
2024-07-18 16:22:08 +1200

    build: ignore some more dart sass warnings

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -35,9 +35,13 @@ module.exports = generateWebpackConfig({
     })
   ],
   ignoreWarnings: [
+    // for some reason we're getting really generic deprecation warnings with
+    // no actual message - suspect it's related to old dependencies
+    /Module Warning \(from \.\/node_modules\/sass-loader\/dist\/cjs\.js\):/u,
     // these primarily come from dependencies like Foundation and are very noisy
     // so lets just exclude them completely until Dart Sass 2 is actually near
     /will be removed in Dart Sass 2\.0\.0/u,
-    /You probably don't mean to use the color value/u
+    /You probably don't mean to use the color value/u,
+    /To opt into the new behavior, wrap the declaration in `& \{\}`/u
   ]
 });
