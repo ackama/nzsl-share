ac2caec25ee6c59e1d2871ae0e77c319b7365b32
Gareth Jones <jones258@gmail.com>
2024-08-02 09:47:39 +1200

    refactor: update stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,15 @@ module.exports = {
   extends: ['stylelint-config-recommended-scss'],
   rules: {
     'no-descending-specificity': null,
+    'string-no-newline': true,
+    'color-no-invalid-hex': true,
+    'comment-whitespace-inside': 'always',
+    'declaration-block-no-duplicate-custom-properties': true,
+    'declaration-block-no-duplicate-properties': true,
+    'no-invalid-double-slash-comments': true,
+    'no-duplicate-at-import-rules': true,
+    'length-zero-no-unit': true,
+    'scss/at-extend-no-missing-placeholder': null,
     'scss/no-global-function-names': null,
     // this does not apply for SCSS
     'no-invalid-position-at-import-rule': null,
